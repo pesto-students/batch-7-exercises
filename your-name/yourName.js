9bc09dadc6eceed08c52e2f6447d5189a44daa92
Arfat Salman <giney.paradise@gmail.com>
2019-05-01 01:53:05 +0530

    Exercises for day-2-segment-1

---
+++ your-name/yourName.js
@@ -0,0 +1,8 @@
+
+function yourName(...args) {
+  return args;
+}
+
+export {
+  yourName,
+};
---
+++ your-name/yourName.test.js
@@ -0,0 +1,7 @@
+import { yourName } from './yourName';
+
+describe('Write your name in the test', () => {
+  test('yourName should return my name', () => {
+    expect(yourName()).toBe('Write your name here!');
+  });
+});
