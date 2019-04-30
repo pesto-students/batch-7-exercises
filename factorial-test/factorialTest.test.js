8ebc64fc69f2d67690980b5b1e6b274338e93d0e
VikalpP <vikalppanseriya@gmail.com>
2019-05-01 05:06:29 +0530

    Solve All Tests

--- factorial-test/factorialTest.test.js
+++ factorial-test/factorialTest.test.js
@@ -18,7 +18,9 @@ describe('factorial test', () => {
   });
 
   test('should find factorial of 0', () => {
-    // delete next line and replace it with proper test
-    expect(true).toEqual(false);
+    const actual = factorial(0);
+    const expected = 1;
+
+    expect(actual).toEqual(expected);
   });
 });
