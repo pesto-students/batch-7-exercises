87642651093a65d3292178df91557b4a96b368b2
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-02 04:57:59 +0530

    Solve diffArray

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,8 +1,12 @@
-
-function diffArray(...args) {
-  return args;
+function removeCommonElem(array1, array2) {
+  return array1.filter(value => !array2.includes(value));
 }
 
-export {
-  diffArray,
-};
+function diffArray(array1, array2) {
+  const tempArray = array1;
+  const uniqueArr1 = removeCommonElem(array1, array2);
+  const uniqueArr2 = removeCommonElem(array2, tempArray);
+  return uniqueArr1.concat(uniqueArr2);
+}
+
+export { diffArray };
