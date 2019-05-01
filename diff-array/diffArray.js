ee55ee2072c555edbf377b5778370ed829aa0cf2
akshit <ajindal126@gmail.com>
2019-05-02 04:50:46 +0530

    solved diff-array

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,9 @@
 
 function diffArray(...args) {
-  return args;
+	const array1=args[0];
+	const array2=args[1];
+	let returnedArray= array1.filter((element)=> array2.indexOf(element)===-1)
+	return returnedArray.concat(array2.filter((element)=> array1.indexOf(element)===-1))
 }
 
 export {
