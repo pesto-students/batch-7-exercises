2cc0915527d3f55e4adbb982598191dd5dd7b1e3
VikalpP <vikalppanseriya@gmail.com>
2019-05-02 04:34:53 +0530

    Solve bouncer

--- bouncer/bouncer.js
+++ bouncer/bouncer.js
@@ -1,6 +1,6 @@
 
-function bouncer(...args) {
-  return args;
+function bouncer(array) {
+  return array.filter(element => (typeof element === 'string' || typeof element === 'number') && element);
 }
 
 export {
