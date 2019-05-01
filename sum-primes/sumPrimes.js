401562f49cc5551d7383ba9b38d6875e0a698640
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-02 00:55:45 +0530

    Solve sum-primes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,23 @@
+import { isPrimitive } from "jest-get-type";
 
-function sumPrimes(...args) {
-  return args;
+function sumPrimes(n) {
+  let sum = 0 ;
+  for (let i=2; i<=n; i++) {
+    if (isPrime(i)) {
+      console.log(i);
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+function isPrime(n) {
+  for (let i = 2;i < n; i++ ) {
+    if (n%i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export {
