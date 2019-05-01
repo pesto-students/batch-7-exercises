209b8ad6c608f4bc86f759f81316fb8ce330cfd0
akshit <ajindal126@gmail.com>
2019-05-02 04:32:12 +0530

    solved binary-agent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,13 @@
 
 function binaryAgent(...args) {
-  return args;
+  var binaryString=args[0];
+  var binaryStringArray=binaryString.split(' ');
+  var englishString=[];
+  for(let i=0;i<binaryStringArray.length;i++){
+  	englishString.push(String.fromCharCode(parseInt(binaryStringArray[i],2)));
+  }
+
+  return englishString.join("");
 }
 
 export {
