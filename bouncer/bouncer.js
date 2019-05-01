7291726f8e94be3a36953cae6da2cd0fda5c648e
manish zanzad <manishzanzad@gmail.com>
2019-05-02 04:55:33 +0530

    Solve excersice binary-agent and bouncer

--- bouncer/bouncer.js
+++ bouncer/bouncer.js
@@ -1,6 +1,21 @@
 
-function bouncer(...args) {
-  return args;
+function bouncer(array) {
+  var falsyValueArray = [false, null, 0, undefined, ''];
+  falsyValueArray.forEach(falseValue => {
+    while(array.length > 0) {
+      var index = array.indexOf(falseValue);
+      if(index==-1)
+        break;
+        array.splice(index,1);
+    }
+  });
+  //  Remove NaN from array
+  for (var i = 0; i < array.length; i++) {
+      if(Number.isNaN(array[i])) {
+        array.splice(i,1);
+      }
+  }
+  return array;
 }
 
 export {
