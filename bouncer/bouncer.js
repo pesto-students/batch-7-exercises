909a0f7291f22e3947a162d58584497371f4d7a6
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-02 04:55:23 +0530

    Solve bouncer problem

--- bouncer/bouncer.js
+++ bouncer/bouncer.js
@@ -1,6 +1,12 @@
 
 function bouncer(...args) {
-  return args;
+  let inputArray = args[0];
+  let answerArray = inputArray.filter(function(element) {
+    if (element != false) {
+      return element;
+    }
+  });
+  return answerArray;
 }
 
 export {
