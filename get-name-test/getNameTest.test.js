45be51a9ca37e6f3d43864bda365dbf7b854dab8
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-01 08:25:36 +0530

    Solve get name test

--- get-name-test/getNameTest.test.js
+++ get-name-test/getNameTest.test.js
@@ -7,12 +7,12 @@ function getName(name) {
 describe('throwError test', () => {
   test('should return string with given name', () => {
     const actual = getName('Martin Garrix');
-    const expected = /Ramu/;
+    const expected = 'You entered: Martin Garrix';
     expect(actual).toMatch(expected);
   });
 
   test('should throw error if argument is not a string', () => {
-    const expected = 'there was an error';
+    const expected = new Error('name should be of type string');
     expect(() => getName(123)).toThrow(expected);
   });
 });
