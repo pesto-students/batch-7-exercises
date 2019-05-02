fff0fd7b928191333e471cc7fdb366220f1e8e47
VikalpP <vikalppanseriya@gmail.com>
2019-05-02 07:37:08 +0530

    solve isPalindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,8 +1,23 @@
+function isPalindrome(inputString) {
+  // Removes symbols [ '_', '-', '/', ' ' ] from inputString
+  const filteredInputString = inputString
+    .toString()
+    .replace(/[_,-/\s+]/g, '')
+    .replace(')', '(');
 
-function isPalindrome(...args) {
-  return args;
+  const lastIndexOfString = filteredInputString.length - 1;
+
+  for (let i = 0; i < filteredInputString.length / 2; i += 1) {
+    const char1 = filteredInputString.charAt(i).toUpperCase();
+    const char2 = filteredInputString
+      .charAt(lastIndexOfString - i)
+      .toUpperCase();
+
+    if (char1 !== char2) {
+      return false;
+    }
+  }
+  return true;
 }
 
-export {
-  isPalindrome,
-};
+export { isPalindrome };
