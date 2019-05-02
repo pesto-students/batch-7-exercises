8cc72e1a0035532e11fb7800d84f7d5450c49a3b
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-03 00:36:19 +0530

    Solve mapObject

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,8 +1,8 @@
-
-function mapObject(...args) {
-  return args;
+function mapObject(obj, mappingFunction) {
+  for (let key in obj) {
+    obj[key] = mappingFunction(obj[key]);
+  }
+  return obj;
 }
 
-export {
-  mapObject,
-};
+export { mapObject };
