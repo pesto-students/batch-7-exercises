96301de3ff4bf5968243c543c58591d9b30f779f
akshit <ajindal126@gmail.com>
2019-05-03 00:48:18 +0530

    solved mapObject

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,10 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(obj,cb) {
+  let finalObject={};
+  for(let key in obj){
+  	finalObject[key]=cb(obj[key]);
+  }
+  return finalObject;
 }
 
 export {
