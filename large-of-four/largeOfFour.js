b91f08c278ea30c46ba219d3da388cf232b1e97c
Pratiyush <pkpratiyush@gmail.com>
2019-05-02 08:44:01 +0530

    Solving exercises day-3 segment-2

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,8 +1,27 @@
 
 function largeOfFour(...args) {
-  return args;
+  var arr1 = args[0][0];
+  var arr2 = args[0][1];
+  var arr3 = args[0][2];
+  var arr4 = args[0][3];
+  //console.log(arr2);
+  var finalArray = [findMax(arr1), findMax(arr2), findMax(arr3), findMax(arr4)];
+  return finalArray;
+}
+function findMax(a){
+  //console.log(a);
+  var max = a[0];
+  for( var i = 0; i < a.length ; i++){
+    if(a[i] > max){
+      max = a[i];
+    }
+  }
+  return max;
 }
 
 export {
   largeOfFour,
 };
+// largeOfFour([
+//   [13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1],
+// ]);
--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,6 +1,13 @@
 
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(args) {
+  var strSplit = args.split(' ');
+  var longestWord = 0;
+  for(var i = 0; i < strSplit.length; i++){
+    if(strSplit[i].length > longestWord){
+	longestWord = strSplit[i].length;
+     }
+  }
+  return longestWord;
 }
 
 export {
