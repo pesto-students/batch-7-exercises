9681a93d7695d299071d1671b3e2bda31651298c
akshit <ajindal126@gmail.com>
2019-05-03 02:12:27 +0530

    solved truthCheck

--- truth-check/truthCheck.js
+++ truth-check/truthCheck.js
@@ -1,6 +1,13 @@
 
-function truthCheck(...args) {
-  return args;
+function truthCheck(array,keyToBeChecked) {
+	for(let i=0;i<array.length;i++){
+		for(let key in array[i]){
+			if(!array[i][keyToBeChecked]){
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 export {
