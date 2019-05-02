cdb3aa9cbe7c1abc09b10a8e319dbaad678ebb8f
akshit <ajindal126@gmail.com>
2019-05-02 18:59:43 +0530

    solved large-of-four

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,13 @@
 
 function isPalindrome(...args) {
-  return args;
+	var string=args[0];
+	const mid=Math.floor(string.length/2);
+	for(let i=0;i<mid;i++){
+		if(string[i]!== string[string.length-1-i]){
+			return false;
+		}
+	}
+	return true;
 }
 
 export {
--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,6 +1,12 @@
 
 function largeOfFour(...args) {
-  return args;
+	let arrays=args[0];
+	return arrays.map((array)=> {
+		array.sort(function(a, b) {
+	    	return a - b;
+	    });
+		return array[array.length-1]
+	});
 }
 
 export {
