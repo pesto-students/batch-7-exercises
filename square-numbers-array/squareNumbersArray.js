59c576a9ca0117e5192493e3e8173d8baa3ab057
Pratiyush <pkpratiyush@gmail.com>
2019-05-03 04:52:03 +0530

    Solving Day-4 Segment-2 Exercises

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,8 +1,21 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(...arr) {
+
+  if( (arr.every( isANumber )) === false ){
+    throw Error('My custom error');
+  } 
+}
+function isANumber(num){
+  if(typeof(num) === 'number'){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 export {
   squareNumbersArray,
 };
+// const myArray = [1, 2, 'string', 4];
+// squareNumbersArray(...myArray);
