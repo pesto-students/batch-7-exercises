360d42919cb3f0995fa9bbc2e193382e62172e95
VikalpP <vikalppanseriya@gmail.com>
2019-05-03 00:47:06 +0530

    Solve objectInvert

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,8 +1,10 @@
-
-function objectInvert(...args) {
-  return args;
+function objectInvert(Obj) {
+  const outputObj = {};
+  Object.keys(Obj).forEach((property) => {
+    const propValue = Obj[property];
+    outputObj[propValue] = property;
+  });
+  return outputObj;
 }
 
-export {
-  objectInvert,
-};
+export { objectInvert };
