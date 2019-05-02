9b1c04cb007edd693377eb8b82439454733f2173
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-03 05:22:46 +0530

    Solve partial account-data-set

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,36 @@
+const dataset = require('./dataset.json');
 
-function accountDataset(...args) {
-  return args;
+function hundredThousandairs() {
+  return dataset.bankBalances.filter((account) => account.amount > 100000);
 }
 
+function datasetWithRoundedDollar() {
+  let dataSetModified = dataset.bankBalances.map((account)=>({
+    ...account,
+    rounded : Math.round(account.amount)
+  }));
+  return dataSetModified;
+}
+
+function sumOfBankBalances() {
+  return dataset.bankBalances.reduce((sum,account) =>(sum + Math.round(account.amount)),0)
+}
+
+function sumOfInterests() {
+  let stateForInteresetCalculation = ['WI','IL','WY','OH','GA','DE'];
+  let accountsInInterestedState= dataset.bankBalances.filter((account)=> stateForInteresetCalculation.includes(account.state));
+  accountsInInterestedState.map((account) => (account.amount = parseFloat(account.amount * (18.9 /100)));
+  let sumOfInterests = accountsInInterestedState.reduce((sum,account) => (sum += account.amount),0);
+  return sumOfInterests;
+}
+
+function higherStateSums() {
+  
+}
 export {
-  accountDataset,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances,
+  sumOfInterests,
+  higherStateSums
 };
