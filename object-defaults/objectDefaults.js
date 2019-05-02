fd67baf8b5e02ecafdbda055a56c991e8990c222
akshit <ajindal126@gmail.com>
2019-05-03 00:57:59 +0530

    solved objectDefault

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,6 +1,15 @@
 
-function objectDefaults(...args) {
-  return args;
+function objectDefaults(obj,defaultObj) {
+  let finalObject={};
+  for(let key in obj){
+  	for(let key1 in defaultObj){
+  		if(key===key1){
+  			delete defaultObj[key1];
+  		}
+  	}
+  }
+
+  return {...obj,...defaultObj};
 }
 
 export {
