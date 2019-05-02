8efc569125264a62b47d972e2f73cdfac95b978c
manish zanzad <manishzanzad@gmail.com>
2019-05-03 00:23:21 +0530

    Solve chunk-array-in-groups and drop-elements

--- drop-elements/dropElements.js
+++ drop-elements/dropElements.js
@@ -1,8 +1,11 @@
-
-function dropElements(...args) {
-  return args;
+function dropElements(numericArray, callback) {
+  var dropElementsArray = [];
+  numericArray.forEach(value => {
+    if (callback(value)) {
+      dropElementsArray.push(value);
+    }
+  });
+  return dropElementsArray;
 }
 
-export {
-  dropElements,
-};
+export { dropElements };
