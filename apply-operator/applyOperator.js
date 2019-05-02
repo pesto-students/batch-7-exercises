dd975fcb7761e85dc7053aab0b1d42c2e7fe26dd
amitbadala <amitbadala07@gmail.com>
2019-05-03 00:08:30 +0530

    Solve apply opertor

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,77 @@
 
-function applyOperator(...args) {
-  return args;
+function applyOperator(operator,operands) {
+  var result = 0;
+  
+  if(operands == undefined)
+  {
+    return 0;
+  } 
+  
+  switch(expression) {
+    case '+':
+    {
+      for(var i=0;i<operands.length;i++)
+      {
+        result = result + operands[i];
+      }
+      break;
+    } 
+    case '-':
+    {
+      for(var i=0;i<operands.length;i++)
+      {
+        result = result - operands[i];
+      }
+      break;
+    }
+    case '%':
+    {
+      for(var i=0;i<operands.length;i++)
+      {
+        if(i==0)
+        {
+          result = operands[i];
+        }
+        else
+        {
+          result = result % operands[i];
+        } 
+      }
+      break;
+    }
+    case '/':
+    {
+      if(i==0)
+      {
+        result = operands[i];
+      }
+      else
+      {
+        result = result / operands[i];
+      } 
+      break;
+    }
+    case '*':
+    {
+      for(var i=0;i<operands.length;i++)
+      {
+        if(i==0)
+        {
+          result = operands[i];
+        }
+        else
+        {
+          result = result * operands[i];
+        } 
+      }
+      break;
+    }
+    default:
+     {
+      throw Error;
+     }
+  }
+  return result;
 }
 
 export {
