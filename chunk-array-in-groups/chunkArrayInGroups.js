1925b8a8ac0cc5da6b681a3983d7724c20f8f4d0
Pratiyush <pkpratiyush@gmail.com>
2019-05-03 01:00:58 +0530

    Solving Day-4-Segment-1 Exercises

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,8 +1,29 @@
 
 function chunkArrayInGroups(...args) {
-  return args;
+  var arr = args.flat();
+  var size = arr[arr.length-1];
+  var temp = [];
+  var finalArray = [];
+
+  for (var a = 0; a < arr.length -1 ; a++) {
+    if (a % size !== size - 1)
+      temp.push(arr[a]);
+    else {
+      temp.push(arr[a]);
+      finalArray.push(temp);
+      temp = [];
+    }
+  }
+
+  if (temp.length !== 0)
+    finalArray.push(temp);
+  return finalArray;
 }
+
+
+ 
 
 export {
   chunkArrayInGroups,
 };
+//chunkArrayInGroups(['a', 'b', 'c', 'd'], 2);
--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,8 +1,23 @@
 
 function findElement(...args) {
-  return args;
+  
+  var arr = args[0];
+  var f = 0;
+  console.log(args.length);
+  for( var i = 0; i< args[0].length; i++){
+    var a = args[1](arr[i]);
+    console.log(a);
+    if( a === true){
+      return arr[i];
+      f=1;
+    }
+    }
+    if( f=== 0)
+    return undefined;
 }
 
 export {
   findElement,
 };
+// findElement([1, 3, 5, 8, 9, 10], (num) => {
+//   return num % 2 === 0 });
