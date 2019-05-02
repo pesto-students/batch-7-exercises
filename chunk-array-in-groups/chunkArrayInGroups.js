dff0f9fc8314010897530414c0a07339147b0297
amitbadala <amitbadala07@gmail.com>
2019-05-03 01:01:36 +0530

    Solve ChuckArrayInGroups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,27 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(arr,groupSize) {
+  var groupedArray = [];
+  var masterArrays = [];
+  var noOfGroups = Math.floor(arr.length/groupSize);
+  var lowerBound = 0;
+  var upperBound = groupSize; 
+  while(noOfGroups!=0)
+  {
+    groupedArray = arr.slice(lowerBound,upperBound);
+    masterArrays.push(groupedArray);
+    noOfGroups--;
+    // Push the remaining elements of array
+    if(noOfGroups==0&&(upperBound!=arr.length))
+    {
+      masterArrays.push(arr.slice(upperBound,arr.length));
+    }
+    else
+    { 
+      lowerBound = upperBound;
+      upperBound = upperBound + upperBound; 
+    }
+  } 
+  return masterArrays;
 }
 
 export {
