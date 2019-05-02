2052e1a34d00451fc442b3ac9279356dd9a6db9a
akshit <ajindal126@gmail.com>
2019-05-03 00:24:12 +0530

    solved findElements

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,6 +1,11 @@
 
-function findElement(...args) {
-  return args;
+function findElement(array,cb) {
+  for(let i=0;i<array.length;i++){
+  	if(cb(array[i])){
+  		return array[i];
+  	}
+  }
+  return undefined;
 }
 
 export {
