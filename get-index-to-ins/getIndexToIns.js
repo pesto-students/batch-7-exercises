a83f7f873e86d7336460c80185a8186d6e1fcd4d
akshit <ajindal126@gmail.com>
2019-05-02 18:29:31 +0530

    solved get-index-to-ins

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,18 @@
 
 function getIndexToIns(...args) {
-  return args;
+  let elements=args[0];
+  const number=args[1];
+  elements.sort(function(a, b) {
+    return a - b;
+  });
+  var index=undefined;
+  for(let i=0;i<elements.length;i++){
+  	if(number<=elements[i]){
+  		return i;
+  	}
+  	index=i;
+  }
+  return index+1;
 }
 
 export {
