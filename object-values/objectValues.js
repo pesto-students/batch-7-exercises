5cd87c02f9803e1a03e3182a4c3dc203787555e4
akshit <ajindal126@gmail.com>
2019-05-03 01:43:38 +0530

    solved objectValues

--- object-values/objectValues.js
+++ object-values/objectValues.js
@@ -1,6 +1,14 @@
 
-function objectValues(...args) {
-  return args;
+function objectValues(obj) {
+  let finalArray=[];
+  for(let key in obj){
+  	if(key=== obj[key]){
+  		finalArray.push(key);
+  	}else{
+  		finalArray.push(null);
+  	}
+  }
+  return finalArray;
 }
 
 export {
