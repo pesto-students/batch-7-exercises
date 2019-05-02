56531c43598ba1f4a3b367769bf6d682c8d18f8c
Abhishek <suhadevvenkatesh@gmail.com>
2019-05-03 05:15:46 +0530

    Solve alternatingCharacters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,17 @@
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(arr) {
+  arr.map((element)=>{
+        var characterArray =  element.split()
+        var previousCharacter
+        var charExtractionCount = 0
+        characterArray.map((character)=>{
+            if(character == previousCharacter){
+               charExtractionCount += 1
+               previousCharacter = character
+            }
+            return charExtractionCount
+        })
+  })
 }
 
 export {
