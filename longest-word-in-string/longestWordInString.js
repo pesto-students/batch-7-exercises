1bf782887caa4c9acde90aa55be0719e3491574e
Abhishek <suhadevvenkatesh@gmail.com>
2019-05-02 21:01:05 +0530

    Solve largeOfFour

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,6 +1,14 @@
+import { max } from "date-fns";
 
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(stringValue) {
+  var stringArray = stringValue.split(" ")
+  var maxLength = 0
+  for( let i=0; i < stringArray.length; i++){
+      if(stringArray[i].length > maxLength){
+        maxLength = stringArray[i].length
+      }
+  }
+  return  maxLength
 }
 
 export {
