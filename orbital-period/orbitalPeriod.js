b43bc2b1297e1b35585ec6577111db655f5a29eb
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-03 01:10:38 +0530

    Solve orbitalPeriod

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,8 +1,17 @@
+function orbitalPeriod(satellites) {
+  // Constant for Kepler's Third Law
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
 
-function orbitalPeriod(...args) {
-  return args;
+  satellites.forEach((satellite) => {
+    // Using Kepler's Third Law
+    const a3 = (satellite.avgAlt + earthRadius) ** 3;
+    const period = 2 * Math.PI * Math.sqrt(a3 / GM);
+    satellite.orbitalPeriod = Math.round(period);
+    delete satellite.avgAlt;
+  });
+
+  return satellites;
 }
 
-export {
-  orbitalPeriod,
-};
+export { orbitalPeriod };
