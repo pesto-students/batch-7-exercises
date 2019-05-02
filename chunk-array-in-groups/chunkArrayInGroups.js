d253d3f5e38e0d9aff814586aedb57ccffde9bd4
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-03 00:18:08 +0530

    Solve chunkArrayInGroups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,8 +1,9 @@
-
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(array, splitIn) {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += splitIn) {
+    chunks.push(array.slice(i, i + splitIn));
+  }
+  return chunks;
 }
 
-export {
-  chunkArrayInGroups,
-};
+export { chunkArrayInGroups };
