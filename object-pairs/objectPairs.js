e176cd3474e75b757af9930bb2b994d4a527d147
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-03 00:50:13 +0530

    Solve objectPairs

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,8 +1,12 @@
-
-function objectPairs(...args) {
-  return args;
+function objectPairs(obj) {
+  const arrayOfPairs = [];
+  for (const key in obj) {
+    const pair = [];
+    pair.push(key);
+    pair.push(obj[key]);
+    arrayOfPairs.push(pair);
+  }
+  return arrayOfPairs;
 }
 
-export {
-  objectPairs,
-};
+export { objectPairs };
