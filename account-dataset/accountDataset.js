cfa33e80ef0b48c2dcf3ae396fa4d090bc42c0d2
VikalpP <vikalppanseriya@gmail.com>
2019-05-03 05:25:57 +0530

    working on accountDataset

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,61 @@
+const dataset = require('./dataset.json');
 
-function accountDataset(...args) {
-  return args;
+const { bankBalances } = dataset;
+
+function hundredThousandairs() {
+  return bankBalances.filter(account => account.amount > 100000);
+}
+
+function datasetWithRoundedDollar() {
+  return bankBalances.map((account) => {
+    const modifiedAccount = account;
+    const roundedAmount = Math.round(account.amount);
+    modifiedAccount.rounded = roundedAmount;
+    return modifiedAccount;
+  });
+}
+
+function sumOfBankBalances() {
+  return bankBalances.reduce((totalAmount, account) => totalAmount + account, 0);
+}
+
+function addInterest({ account, percentage }) {
+  const modifiedAccount = account;
+  modifiedAccount.amount += account.amount * percentage;
+  return modifiedAccount;
+}
+
+function sumOfInterests() {
+  const states = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+
+  const filteredAccounts = bankBalances.filter(account => states.includes(account.state));
+
+  // Adds 18.9% interest to it & rounds to the nearest cents
+  const totalInterests = filteredAccounts.reduce((total, account) => {
+    const modifiedAccount = addInterest({ account, percentage: 18.9 });
+    return total + modifiedAccount.amount;
+  }, 0);
+  return totalInterests;
+}
+
+function higherStateSums() {
+  const states = [];
+  bankBalances.forEach((account) => {
+    const { state } = account;
+    const amount = parseInt(account.amount, 10);
+    if (states[state] === undefined) {
+      states[state] = amount;
+    } else {
+      states[state] += amount;
+    }
+  });
+  
 }
 
 export {
-  accountDataset,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances,
+  sumOfInterests,
+  higherStateSums,
 };
