c338b2cc58e692fd7d0602531fe7a6662e5a00d4
manish zanzad <manishzanzad@gmail.com>
2019-05-03 00:40:48 +0530

    Solve excersices find-element and map-object

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,8 +1,10 @@
-
-function findElement(...args) {
-  return args;
+function findElement(numericArray, callback) {
+  for (var i = 0; i < numericArray.length; i++) {
+    var element = numericArray[i];
+    if (callback(element)) {
+      return element;
+    }
+  }
 }
 
-export {
-  findElement,
-};
+export { findElement };
--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,8 +1,8 @@
-
-function mapObject(...args) {
-  return args;
+function mapObject(object, callback) {
+  for (var key in object) {
+    object[key] = callback(object[key]);
+  }
+  return object;
 }
 
-export {
-  mapObject,
-};
+export { mapObject };
