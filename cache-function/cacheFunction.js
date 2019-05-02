70af2af5955cf6d051c8002d7c4d5b4bf4a7fe89
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-03 00:08:00 +0530

    Solve cacheFunction

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,8 +1,19 @@
+const cache = [];
 
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(inputFunction) {
+  return (args) => {
+    const found = cache.find(object => object.input === args);
+    if (found) {
+      return found.result;
+    }
+
+    const func = inputFunction(args);
+    cache.push({
+      input: args,
+      result: func,
+    });
+    return func;
+  };
 }
 
-export {
-  cacheFunction,
-};
+export { cacheFunction };
