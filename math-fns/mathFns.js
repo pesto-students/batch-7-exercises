f92d2832b4a6a0bebd6cff1913baf2bf1dfc7692
Abhishek <suhadevvenkatesh@gmail.com>
2019-05-04 01:43:32 +0530

    Solve daysInMonth

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -1,7 +1,14 @@
 
-function mathFns(...args) {
-  return args;
-}
+ var sqrt = function(inputNumber){
+    return Math.sqrt(inputNumber)
+  }
+ var power = function(baseNumber,power){
+    return Math.pow(baseNumber,power)
+  }
+  var round = function(floatValue){
+    return Math.floor(floatValue)
+  }
+
 
 export {
   mathFns,
