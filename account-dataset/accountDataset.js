137ae373702a5e8d8afbbf3c1966129d7b1d6c96
akshit <ajindal126@gmail.com>
2019-05-03 05:30:03 +0530

    solved accountDataset

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,60 @@
+let parsedDataset = require('./dataset.json').bankBalances;
+// let parsedDataset=JSON.parse(dataset);
+function hundredThousandairs() {
+	return parsedDataset.filter((data)=>data.amount>100000);
+}
 
-function accountDataset(...args) {
-  return args;
+function datasetWithRoundedDollar() {
+	return parsedDataset.map((data)=>{
+		return {
+			amount  : data.amount,
+			state   : data.state,
+			rounded : Math.ceil(data.amount)
+		}
+	});
+}
+
+function sumOfBankBalances() {
+	let sumOfAmount=0;
+	parsedDataset.forEach((data)=>{
+		sumOfAmount += data.amount;
+	});
+	return sumOfAmount;
+}
+
+function sumOfInterests() {
+	let sumOfInterests=0;
+	parsedDataset.forEach((data)=>{
+		if(['WI','IL','WY','OH','GA','DE'].includes(data.state)){
+			sumOfInterests += data.amount + (data.amount * 0.189)
+		}
+	});
+	return sumOfInterests;
+}
+
+function higherStateSums() {
+	var higherStateSums=0;
+	let stateObject={};
+	parsedDataset.forEach((data)=>{
+		var state=data.state;
+		if(!stateObject[state]){
+			stateObject[state]=data.amount;
+		}else{
+			stateObject[state] += data.amount;
+		}
+	});
+	Object.keys(stateObject).forEach(key =>{
+		if(stateObject[key] > 1000000){
+			higherStateSums +=stateObject[key];
+		}
+	});
+	return higherStateSums;
 }
 
 export {
-  accountDataset,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances,
+  sumOfInterests,
+  higherStateSums
 };
--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,5 @@
 
 function alternatingCharacters(...args) {
-  
   return args;
 } 
 
