40c80de4e68ecaadb0a23a7cb7ba26cda66b5c71
Pratiyush <pkpratiyush@gmail.com>
2019-05-04 00:25:23 +0530

    Solving Day-5-Segment-1 Exercises

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,8 +1,18 @@
-
-function addBigIntegers(...args) {
-  return args;
+//[WIP]:
+function addBigIntegers(arr) {
+  var length = arr[0];
+  //console.log(length);
+  //addIntegers(arr,length);
+  //console.log(addIntegers(arr,length));
+  var sum = 0;
+  
+  
 }
 
-export {
-  addBigIntegers,
-};
+// export {
+//   addBigIntegers,
+// };
+addBigIntegers(`3
+539
+8
+201`);
