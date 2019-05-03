8c18993bf5492d64580061db6e42543db9d40660
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-04 00:51:14 +0530

    Solve json-data

--- json-parse/json-data.js
+++ json-parse/json-data.js
@@ -1,15 +1,14 @@
-
 const clubMember = {
   firstname: 'John',
   lastname: 'Doe',
   memberStatus: 'Full',
 };
+clubMember.city = 'New York';
 
 const Sara = '{ "height":1.9, "age":36, "eyeColor":"brown"}';
 
 // use JSON.parse() to create an object 'objectSara':
-const objectSara = `${Sara} add code here`;
-
+const objectSara = JSON.parse(Sara); // `${Sara} add code here`;
 
 // var myArray = [];
 // myArray[0] = 'Monday';
@@ -31,8 +30,7 @@ const conference = {
 // You can access the individual properties of the object as if they were elements
 // in an associative array: `conference['startDay']`
 // Add a line of code to store the name of the conference's end day in variable endDayIs.
-const endDay = conference;
-
+const { endDay } = conference;
 
 const Mike = {};
 Mike.height = 1.9;
@@ -40,11 +38,8 @@ Mike.age = 36;
 Mike.eyecolor = 'brown';
 
 // serialize the object using JSON.stringify():
-const mikeStringified = Sara;
+const mikeStringified = JSON.stringify(Mike);
 
 export {
-  clubMember,
-  objectSara,
-  endDay,
-  mikeStringified,
+  clubMember, objectSara, endDay, mikeStringified,
 };
