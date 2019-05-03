64297140f466c7dbdb0d68d3fed9e05cac6033ca
Abhishek <suhadevvenkatesh@gmail.com>
2019-05-04 00:35:41 +0530

    Solve abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,26 @@
+try{
+function abbreviateString(stringToAbbreviate) {
+  var splittedStringArray = stringToAbbreviate.split(" ")
+  var abrreviatedString = ""
+  if(typeof abbreviateString.arguments == "number" || abbreviateString.arguments == "null" )
+  {
+    throw Error("Not a valid input")
+  }
+  splittedStringArray.map((element)=>{
+     if(splittedStringArray.indexOf(element) = 0){
+       abbreviateString += element
+     }
+     else{
+       if(splittedStringArray.indexOf(element) = splittedStringArray.length){
+         abbreviateString += element.substring(0,1)
+       }
+     }
+  })
+}
 
-function abbreviateString(...args) {
-  return args;
+}
+catch(e){
+ return e
 }
 
 export {
