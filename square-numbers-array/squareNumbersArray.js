ab35c1822128edffe733b447da09dfff2d92457b
amitbadala <amitbadala07@gmail.com>
2019-05-04 00:08:03 +0530

    Solve square numbers array

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,21 @@
+import { throwError } from "rxjs";
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(arr) {
+  
+    var newArray =  arr.map(squareNumber);
+    return newArray;
+  } 
+
+function squareNumber(n){
+  if(isNumber(n))
+  { 
+    return Math.pow(n,2);
+  } 
+}
+
+function isNumber(elements)
+{
+  return typeof elements === 'Number'
 }
 
 export {
