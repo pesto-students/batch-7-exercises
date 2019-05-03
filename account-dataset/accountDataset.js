9ca7000a562be66a27f8e728430e7b526d0431d3
VikalpP <vikalppanseriya@gmail.com>
2019-05-03 08:01:47 +0530

    Solve accountDataset

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -7,49 +7,52 @@ function hundredThousandairs() {
 }
 
 function datasetWithRoundedDollar() {
-  return bankBalances.map((account) => {
-    const modifiedAccount = account;
-    const roundedAmount = Math.round(account.amount);
-    modifiedAccount.rounded = roundedAmount;
-    return modifiedAccount;
-  });
+  return bankBalances.map(({ amount, ...rest }) => ({
+    amount,
+    rounded: Math.round(amount),
+    ...rest,
+  }));
 }
 
 function sumOfBankBalances() {
-  return bankBalances.reduce((totalAmount, account) => totalAmount + account, 0);
-}
-
-function addInterest({ account, percentage }) {
-  const modifiedAccount = account;
-  modifiedAccount.amount += account.amount * percentage;
-  return modifiedAccount;
+  const sum = bankBalances.reduce((totalAmount, { amount }) => totalAmount + parseFloat(amount), 0);
+  return Math.round(sum * 100) / 100;
 }
 
 function sumOfInterests() {
+  const PERCENTAGE_INTEREST = 18.9;
   const states = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
 
   const filteredAccounts = bankBalances.filter(account => states.includes(account.state));
 
   // Adds 18.9% interest to it & rounds to the nearest cents
-  const totalInterests = filteredAccounts.reduce((total, account) => {
-    const modifiedAccount = addInterest({ account, percentage: 18.9 });
-    return total + modifiedAccount.amount;
+  const totalInterests = filteredAccounts.reduce((total, { amount }) => {
+    const interestAmount = parseFloat((amount * PERCENTAGE_INTEREST) / 100);
+    const newAmount = parseFloat(interestAmount + amount).toFixed(2);
+
+    return total + parseFloat(newAmount);
   }, 0);
-  return totalInterests;
+  return parseFloat(totalInterests.toFixed(2));
 }
 
 function higherStateSums() {
+  const THRESOLD_AMOUNT_PER_STATE = 1000000;
   const states = [];
   bankBalances.forEach((account) => {
     const { state } = account;
-    const amount = parseInt(account.amount, 10);
-    if (states[state] === undefined) {
+    const amount = parseFloat(account.amount);
+    if (!states[state]) {
       states[state] = amount;
     } else {
       states[state] += amount;
     }
   });
-  
+
+  const filteredStateAmounts = Object.values(states).filter(
+    amount => amount > THRESOLD_AMOUNT_PER_STATE,
+  );
+
+  return filteredStateAmounts.reduce((finalSum, amount) => finalSum + amount, 0);
 }
 
 export {
