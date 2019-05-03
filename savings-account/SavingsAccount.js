7b847a36441f894dd91a697ff833bb343c160039
Pratiyush <pkpratiyush@gmail.com>
2019-05-03 06:50:30 +0530

    Solving Day-4 Segment-2 exercises

--- savings-account/SavingsAccount.js
+++ savings-account/SavingsAccount.js
@@ -1,8 +1,29 @@
 
-function savingsAccount(...args) {
-  return args;
+function savingsAccount(...accountDetails) {
+  const myAccount = {
+    _accountNumber: accountDetails[0],
+    _email: accountDetails[1],
+    _firstName: accountDetails[2],
+    _lastName: accountDetails[3],
+    products: [],
+  };
+
+  if( myAccount._accountNumber.length != 6 ){
+    throw Error('Account Number must be a 6-digit number');
+  }
+  if( myAccount._firstName.length < 3 || myAccount._firstName.length > 20 ){
+    throw Error('First name must be between 3 and 20 characters long');
+  }
+  if( hasNumber(myAccount._lastName) ){
+    throw Error('Last name must contain english alphabets only');
+  }
+}
+
+function hasNumber(myString) {
+  return /\d/.test(myString);
 }
 
 export {
   savingsAccount,
 };
+//const actualAccount = new savingsAccount('123456', 'kohli@gmail.com', 'Virat', 'Kohli');
