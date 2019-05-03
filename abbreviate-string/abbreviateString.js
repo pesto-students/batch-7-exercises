97fd133a7e712a22dfa517702c7d45fe80a3dd02
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-04 00:51:38 +0530

    Solve abbreviate string,boolean,days,lowercase,math-fns

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,11 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(input) {
+  debugger;
+  let inputWordsArray = input.split(' ');
+  let firsWord = inputWordsArray[0];
+  let lastWord = inputWordsArray[length -1];
+  let lastWordFirstLetter = lastWord.charAt(0).toUpperCase()
+  return firsWord + ' ' + lastWordFirstLetter + '. '
 }
 
 export {
--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,6 +1,29 @@
 
-function daysInMonth(...args) {
-  return args;
+function daysInMonth(year, month) {
+
+  var daysInMonth = {
+    '1' :31,
+    '3' : 31,
+    '4' :30,
+    '5' : 31,
+    '6' : 30,
+    '7' :31,
+    '8':31,
+    '9' :30,
+    '10' : 31,
+    '11' : 30,
+    '12' : 31
+  }
+  var isLeapYear = (year % 4 === 0) && (year % 100 !=0);
+  if (isLeapYear) {
+    if (month == 2) {
+      return 29
+    } else {
+      return 28
+    }
+  } else {
+    return daysInMonth[month];
+  }
 }
 
 export {
