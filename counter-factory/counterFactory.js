6a8f10dc5fc9c23268eb5724607af39e3cad9c01
VikalpP <vikalppanseriya@gmail.com>
2019-05-03 08:36:31 +0530

    Solve counterFactory

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,8 +1,15 @@
-
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  let counter = 0;
+  return {
+    increment: () => {
+      counter += 1;
+      return counter;
+    },
+    decrement: () => {
+      counter -= 1;
+      return counter;
+    },
+  };
 }
 
-export {
-  counterFactory,
-};
+export { counterFactory };
