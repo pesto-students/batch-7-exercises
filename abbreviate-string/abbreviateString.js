eba20710917ce840d24a36455f484a837571aa57
amitbadala <amitbadala07@gmail.com>
2019-05-04 00:41:03 +0530

    Solve abreivateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,7 +1,17 @@
 
-function abbreviateString(...args) {
-  return args;
-}
+function abbreviateString(stringToAbbreviate) {
+  var firstSpace = stringToAbbreviate.indexOf(" ");
+  var lastSpace = stringToAbbreviate.lastIndexOf(" ");
+  var firstHalf = sliceString(stringToAbbreviate,0,firstSpace);
+  var secondHalf = sliceString(stringToAbbreviate,lastSpace,lastSpace+2).toUpperCase();
+   var abbreviatedString =  firstHalf + secondHalf + '.';
+    return abbreviatedString;
+  }
+
+  function sliceString(stringToSlice,lb,up)
+  {
+    return stringToSlice.slice(lb,up);
+  }
 
 export {
   abbreviateString,
