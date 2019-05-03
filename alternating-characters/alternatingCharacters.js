776aaee465b5ccd899f799200bc365b472c02483
VikalpP <vikalppanseriya@gmail.com>
2019-05-03 08:16:01 +0530

    Solve alternatingCharacters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,8 +1,21 @@
-
-function alternatingCharacters(...args) {
-  return args;
+function getNoOfDeletion(string) {
+  let timesToDelete = 0;
+  for (let i = 1; i < string.length; i += 1) {
+    if (string.charAt(i) === string.charAt(i - 1)) {
+      string.slice(i, i + 1);
+      timesToDelete += 1;
+    }
+  }
+  return timesToDelete;
 }
 
-export {
-  alternatingCharacters,
-};
+function alternatingCharacters(arrayStrings) {
+  const timesToDeleteArray = [];
+  arrayStrings.forEach((string) => {
+    const timesToDelete = getNoOfDeletion(string);
+    timesToDeleteArray.push(timesToDelete);
+  });
+  return timesToDeleteArray;
+}
+
+export { alternatingCharacters };
