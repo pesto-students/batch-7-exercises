c2dead7fb21c33ce97500163ed5cf1b817ac3786
VikalpP <vikalppanseriya@gmail.com>
2019-05-05 01:05:45 +0530

    Solve sumFibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,22 @@
+function isOdd(num) {
+  return num % 2 === 1;
+}
 
-function sumFibs(...args) {
-  return args;
+function sumFibs(number) {
+  let fib1 = 1;
+  let fib2 = 1;
+
+  let sum = 0;
+  while (fib1 < number) {
+    if (isOdd(fib1)) {
+      sum += fib1;
+    }
+
+    const newFib = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = newFib;
+  }
+  return sum;
 }
 
 export {
