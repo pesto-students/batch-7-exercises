38dab335acaae9bfbf405235958f4f0cd4cda497
VikalpP <vikalppanseriya@gmail.com>
2019-05-05 05:40:14 +0530

    Solve longestWordInString

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,6 +1,12 @@
 
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(statement) {
+  const arrayOfWords = statement.split(' ');
+  let longestLen = 0;
+  arrayOfWords.forEach((word) => {
+    const { length } = word;
+    if (longestLen < length) longestLen = length;
+  });
+  return longestLen;
 }
 
 export {
