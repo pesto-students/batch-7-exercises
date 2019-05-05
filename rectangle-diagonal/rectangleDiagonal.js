ca6bcaa05f70b312970434f9c7bd6255bad26b05
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-06 04:46:14 +0530

    Solve rectangleDiagonal

--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -1,8 +1,6 @@
-
-function rectangleDiagonal(...args) {
-  return args;
+function rectangleDiagonal(length, breadth) {
+  // Using Pythagoras formula
+  return Math.sqrt((length ** 2) + (breadth ** 2));
 }
 
-export {
-  rectangleDiagonal,
-};
+export { rectangleDiagonal };
