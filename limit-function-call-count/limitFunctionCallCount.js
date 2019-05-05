1853f53f45c039fb9fd273529a34664517245e09
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-06 04:02:23 +0530

    Solve limitFunctionCallCount

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,8 +1,12 @@
-
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(callback, callLimit) {
+  let callCount = 0;
+  return (...args) => {
+    callCount += 1;
+    if (callCount < callLimit) {
+      return callback(...args);
+    }
+    return null;
+  };
 }
 
-export {
-  limitFunctionCallCount,
-};
+export { limitFunctionCallCount };
