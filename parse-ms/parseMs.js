9660130b65767bc57f3594a3b5a42f81b683044d
manish zanzad <manishzanzad@gmail.com>
2019-05-05 13:28:38 +0530

    Solve excersice math-fns, round-to

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,8 +1,39 @@
+function parseMs(milliSeconds) {
+  var timeObject = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0
+  };
+  var isNegative = milliSeconds >= 0 ? false : true;
+  var millisecInADay = 86400000;
+  var millisecInAHour = 3600000;
+  var millisecInAMin = 60000;
+  var millisecInASec = 1000;
+  timeObject.days =
+    isNegative && milliSeconds === 0
+      ? -0
+      : parseInt(milliSeconds / millisecInADay);
+  milliSeconds = milliSeconds % millisecInADay;
+  timeObject.hours =
+    isNegative && milliSeconds === 0
+      ? -0
+      : parseInt(milliSeconds / millisecInAHour);
+  milliSeconds = milliSeconds % millisecInAHour;
+  timeObject.minutes =
+    isNegative && milliSeconds === 0
+      ? -0
+      : parseInt(milliSeconds / millisecInAMin);
+  milliSeconds = milliSeconds % millisecInAMin;
+  timeObject.seconds =
+    isNegative && milliSeconds === 0
+      ? -0
+      : parseInt(milliSeconds / millisecInASec);
+  timeObject.milliseconds =
+    isNegative && milliSeconds === 0 ? -0 : milliSeconds % millisecInASec;
 
-function parseMs(...args) {
-  return args;
+  return timeObject;
 }
 
-export {
-  parseMs,
-};
+export { parseMs };
