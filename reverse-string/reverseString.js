bb4993086422edf45429ca41822c380ff17ba7ae
VikalpP <vikalppanseriya@gmail.com>
2019-05-05 05:32:35 +0530

    Solve reverseString

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,12 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(str) {
+  let reversedString = '';
+  let { length } = str;
+  while (length) {
+    length -= 1;
+    reversedString += str.charAt(length);
+  }
+  return reversedString;
 }
 
 export {
