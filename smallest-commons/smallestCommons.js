e11acb611bc7b18f587b46f92a67defb35905a7c
manish zanzad <manishzanzad@gmail.com>
2019-05-06 04:02:48 +0530

    solve exerscise orbital period, smallest-commons, update-inventory, user-check-password

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,8 +1,24 @@
-
-function smallestCommons(...args) {
-  return args;
+function GCD(num1, num2) {
+  return !num2 ? num1 : GCD(num2, num1 % num2);
+}
+function LCM(num1, num2) {
+  return (num1 * num2) / GCD(num1, num2);
 }
 
-export {
-  smallestCommons,
-};
+function smallestCommons(numberArry) {
+  var min, max;
+  if (numberArry[0] > numberArry[1]) {
+    min = numberArry[1];
+    max = numberArry[0];
+  } else {
+    min = numberArry[0];
+    max = numberArry[1];
+  }
+  var commonMultiple = min;
+  for (var i = min; i <= max; i++) {
+    commonMultiple = LCM(commonMultiple, i);
+  }
+  return commonMultiple;
+}
+
+export { smallestCommons };
