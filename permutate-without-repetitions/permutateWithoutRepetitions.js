52f9800c2a9e4b9f435ab2f4d8176b65be469787
amitbadala <amitbadala07@gmail.com>
2019-05-06 03:01:01 +0530

    Solve PermuationWithoutRepetition Remaining

--- permutate-without-repetitions/permutateWithoutRepetitions.js
+++ permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -1,6 +1,33 @@
+function factorial(number) {
+  let result = 1;
 
-function permutateWithoutRepetitions(...args) {
-  return args;
+  for (let i = 2; i <= number; i += 1) {
+    result *= i;
+  }
+
+  return result;
+}
+
+function permutateWithoutRepetitions(args) {
+  let newArray = [];
+  let similar = [];
+  const mast = [];
+  similar = args;
+  const factorialNumber = factorial(args.length);
+  for (let i = 0; i < factorialNumber; i += 1) {
+    for (let j = 0; j < similar.length; j += 1) {
+      if (j === similar.length - 1) {
+        newArray[0] = similar[j];
+      } else {
+        newArray[j + 1] = similar[j];
+      }
+    }
+    similar = newArray;
+    mast.push(newArray);
+    newArray = [];
+  }
+  mast.unshift(mast.pop());
+  return mast;
 }
 
 export {
