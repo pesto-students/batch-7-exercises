79fe0c25fd6a691d4145cfe79426298170ae4d43
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-07 00:41:49 +0530

    Solve constImmutable

--- const-immutable/constImmutable.js
+++ const-immutable/constImmutable.js
@@ -1,8 +1,20 @@
-
-function constImmutable(...args) {
-  return args;
+function constImmutable() {
+  const account = {};
+  Object.defineProperties(account, {
+    username: {
+      value: 'pesto',
+    },
+    password: {
+      value: 'initialPassword',
+      writable: false,
+    },
+  });
+  try {
+    account.password = 's3cret';
+  } catch (error) {
+    console.error('You cannot update password.');
+  }
+  return account.password;
 }
 
-export {
-  constImmutable,
-};
+export { constImmutable };
