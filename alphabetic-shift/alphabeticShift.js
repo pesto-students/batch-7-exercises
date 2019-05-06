8423f101798fffb1315fc8028ca8ec5dc7b33f80
manish zanzad <manishzanzad@gmail.com>
2019-05-07 04:37:56 +0530

    Solve excersices all-promises and alphabetic-shift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,17 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(string) {
+  let shiftedString = '';
+  for (const char of string) {
+    const isZAlphabet = char === 'z' || char === 'Z';
+    if (isZAlphabet) {
+      shiftedString += (char === 'z' ? 'a' : 'A');
+    } else {
+      const asciiValue = char.charCodeAt(0);
+      const shiftedChar = String.fromCharCode(asciiValue + 1);
+      shiftedString += shiftedChar;
+    }
+  }
+  return shiftedString;
 }
 
 export {
