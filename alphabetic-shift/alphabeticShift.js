26c39949b4a334184302afda070d700b0056ec2d
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-07 04:22:21 +0530

    Solve alphabeticShift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,8 +1,11 @@
-
-function alphabeticShift(...args) {
-  return args;
+const SHIFT_BY = 1;
+function alphabeticShift(str) {
+  let shiftedString = '';
+  for (const char of str) {
+    const asciiCode = char.charCodeAt();
+    shiftedString += String.fromCharCode(asciiCode + SHIFT_BY);
+  }
+  return shiftedString;
 }
 
-export {
-  alphabeticShift,
-};
+export { alphabeticShift };
