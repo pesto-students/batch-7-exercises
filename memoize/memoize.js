59d718977c8d19efb93d1b4861bc83e0e0a92ab3
amitbadala <amitbadala07@gmail.com>
2019-05-06 06:34:34 +0530

    Wip memoize

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,20 @@
 
-function memoize(...args) {
-  return args;
+const memoize = (fn) => {
+  debugger;
+  let cache = {};
+  return (...args) => {
+    let n = args[0];
+    if (n in cache) {
+      console.log('Fetching from cache', n);
+      return cache[n];
+    }
+    else {
+      console.log('Calculating result', n);
+      let result = fn(n);
+      cache[n] = result; 
+      return result;
+    }
+  }
 }
 
 export {
