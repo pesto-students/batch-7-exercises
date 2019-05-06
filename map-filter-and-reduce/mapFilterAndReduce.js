79dc0c4e94478d88111cd03d17bafb7025456181
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-07 01:48:42 +0530

    Corrected map filter reduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,8 +1,11 @@
 
 function mapFilterAndReduce(array) {
+  debugger;
   let firstNamesWithLengthGreaterThan5 = array.filter(object => object.firstName.length < 5);
   let output = firstNamesWithLengthGreaterThan5.reduce((acc , object) => {
-    return acc[object.firstName] = object.firstName.length;
+    
+    acc[object.firstName] = object.firstName.length;
+    return acc;
   },{});
   return output;
 }
