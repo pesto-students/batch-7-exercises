7a93ba3f5757083ae981a2f68bc51e7c349d926d
manish zanzad <manishzanzad@gmail.com>
2019-05-06 15:25:50 +0530

    Solve excersice alien-alphabet, parme-ms, permutate-with-repetitions, throw-errors, timewas

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,3 +1,40 @@
+// function parseMs(milliSeconds) {
+//   var timeObject = {
+//     days: 0,
+//     hours: 0,
+//     minutes: 0,
+//     seconds: 0,
+//     milliseconds: 0
+//   };
+//   var isNegative = milliSeconds >= 0 ? false : true;
+//   var millisecInADay = 86400000;
+//   var millisecInAHour = 3600000;
+//   var millisecInAMin = 60000;
+//   var millisecInASec = 1000;
+//   timeObject.days =
+//     isNegative && milliSeconds === 0
+//       ? -0
+//       : parseInt(milliSeconds / millisecInADay);
+//   milliSeconds = milliSeconds % millisecInADay;
+//   timeObject.hours =
+//     isNegative && milliSeconds === 0
+//       ? -0
+//       : parseInt(milliSeconds / millisecInAHour);
+//   milliSeconds = milliSeconds % millisecInAHour;
+//   timeObject.minutes =
+//     isNegative && milliSeconds === 0
+//       ? -0
+//       : parseInt(milliSeconds / millisecInAMin);
+//   milliSeconds = milliSeconds % millisecInAMin;
+//   timeObject.seconds =
+//     isNegative && milliSeconds === 0
+//       ? -0
+//       : parseInt(milliSeconds / millisecInASec);
+//   timeObject.milliseconds =
+//     isNegative && milliSeconds === 0 ? -0 : milliSeconds % millisecInASec;
+
+//   return timeObject;
+// }
 function parseMs(milliSeconds) {
   var timeObject = {
     days: 0,
@@ -7,33 +44,26 @@ function parseMs(milliSeconds) {
     milliseconds: 0
   };
   var isNegative = milliSeconds >= 0 ? false : true;
+  if (isNegative) {
+    milliSeconds = -1 * milliSeconds;
+  }
   var millisecInADay = 86400000;
   var millisecInAHour = 3600000;
   var millisecInAMin = 60000;
   var millisecInASec = 1000;
-  timeObject.days =
-    isNegative && milliSeconds === 0
-      ? -0
-      : parseInt(milliSeconds / millisecInADay);
+  timeObject.days = parseInt(milliSeconds / millisecInADay);
   milliSeconds = milliSeconds % millisecInADay;
-  timeObject.hours =
-    isNegative && milliSeconds === 0
-      ? -0
-      : parseInt(milliSeconds / millisecInAHour);
+  timeObject.hours = parseInt(milliSeconds / millisecInAHour);
   milliSeconds = milliSeconds % millisecInAHour;
-  timeObject.minutes =
-    isNegative && milliSeconds === 0
-      ? -0
-      : parseInt(milliSeconds / millisecInAMin);
+  timeObject.minutes = parseInt(milliSeconds / millisecInAMin);
   milliSeconds = milliSeconds % millisecInAMin;
-  timeObject.seconds =
-    isNegative && milliSeconds === 0
-      ? -0
-      : parseInt(milliSeconds / millisecInASec);
-  timeObject.milliseconds =
-    isNegative && milliSeconds === 0 ? -0 : milliSeconds % millisecInASec;
-
+  timeObject.seconds = parseInt(milliSeconds / millisecInASec);
+  timeObject.milliseconds = milliSeconds % millisecInASec;
+  if (isNegative) {
+    for (var key in timeObject) {
+      timeObject[key] = -1 * timeObject[key];
+    }
+  }
   return timeObject;
 }
-
 export { parseMs };
--- time-was/timeWas.js
+++ time-was/timeWas.js
@@ -1,8 +1,60 @@
+function timeWas(oldTime, currentTime) {
+  const SECONDS = 1000;
+  const MINUTES = SECONDS * 60;
+  const HOURS = MINUTES * 60;
+  const DAYS = HOURS * 24;
+  const WEEKS = DAYS * 7;
+  const MONTHS = DAYS * 30;
+  const YEARS = MONTHS * 12;
 
-function timeWas(...args) {
-  return args;
+  var milliSeconds = (!currentTime ? Date.now() : currentTime) - oldTime;
+
+  if (milliSeconds < 1000) {
+    return "just now";
+  }
+  var subtractTime = Math.floor(milliSeconds / YEARS);
+  if (subtractTime !== 0) {
+    return subtractTime == 1
+      ? `${subtractTime} year ago`
+      : `${subtractTime} years ago`;
+  }
+  subtractTime = Math.floor(milliSeconds / MONTHS);
+  if (subtractTime !== 0) {
+    return subtractTime == 1
+      ? `${subtractTime} month ago`
+      : `${subtractTime} months ago`;
+  }
+
+  subtractTime = Math.floor(milliSeconds / WEEKS);
+  if (subtractTime !== 0) {
+    return subtractTime == 1
+      ? `${subtractTime} week ago`
+      : `${subtractTime} weeks ago`;
+  }
+  subtractTime = Math.floor(milliSeconds / DAYS);
+  if (subtractTime !== 0) {
+    return `${subtractTime} days ago`;
+  }
+  subtractTime = Math.floor(milliSeconds / HOURS);
+  if (subtractTime !== 0) {
+    return subtractTime == 1
+      ? `${subtractTime} hour ago`
+      : `${subtractTime} hours ago`;
+  }
+
+  subtractTime = Math.floor(milliSeconds / MINUTES);
+  if (subtractTime !== 0) {
+    return subtractTime == 1
+      ? `${subtractTime} minute ago`
+      : `${subtractTime} minutes ago`;
+  }
+
+  subtractTime = Math.floor(milliSeconds / SECONDS);
+  if (subtractTime !== 0) {
+    return subtractTime == 1
+      ? `${subtractTime} second ago`
+      : `${subtractTime} seconds ago`;
+  }
 }
 
-export {
-  timeWas,
-};
+export { timeWas };
