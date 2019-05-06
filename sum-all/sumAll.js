9567b0763e373a46ebb77c9936087a0c09d68df4
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-06 07:20:16 +0530

    Solve sumAll

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,8 +1,15 @@
-
-function sumAll(...args) {
-  return args;
+function sumAll(arr) {
+  let smallerNum = 0;
+  let largerNum = 1;
+  if (arr[0] > arr[1]) {
+    smallerNum = 1;
+    largerNum = 0;
+  }
+  let sum = 0;
+  for (let i = arr[smallerNum]; i <= arr[largerNum]; i += 1) {
+    sum += i;
+  }
+  return sum;
 }
 
-export {
-  sumAll,
-};
+export { sumAll };
