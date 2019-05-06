9194880126bf361ceac37f51908f54f70e786809
amitbadala <amitbadala07@gmail.com>
2019-05-06 06:16:08 +0530

    Solve diff-array inprogress

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,11 @@
 
-function diffArray(...args) {
-  return args;
+function diffArray(array1,array2) {
+  let mergeArray = array1.concat(array2);
+  let result = []
+  mergeArray.foreach((e1) => array1.foreach((e2) => {
+    console.log(e1,e2);
+  }))
+  // return args;
 }
 
 export {
