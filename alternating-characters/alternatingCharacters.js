2b86f0d9b138abc9e7d1e64536cee3f3297aae20
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-06 09:43:24 +0530

    Solve alternatingCharacters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,8 +1,13 @@
-
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strArr) {
+  return strArr.map((str) => {
+    let count = 0;
+    for (let i = 0; i < str.length - 1; i += 1) {
+      if (str.charAt(i) === str.charAt(i + 1)) {
+        count += 1;
+      }
+    }
+    return count;
+  });
 }
 
-export {
-  alternatingCharacters,
-};
+export { alternatingCharacters };
