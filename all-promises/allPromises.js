7a1f855acd6d04d4b62902cd4656c0f31bc72ed2
amitbadala <amitbadala07@gmail.com>
2019-05-07 04:57:42 +0530

    Solve all promise isPromise timeout

--- all-promises/allPromises.js
+++ all-promises/allPromises.js
@@ -1,6 +1,15 @@
+import { resolve } from "path";
 
-function allPromises(...args) {
-  return args;
+function allPromises(args) {
+  debugger 
+if(args === undefined){
+  return new Promise(function(resolve,reject){
+     resolve();
+  })
+} 
+Promise.all([args[0], args[1], args[2]]).then(values => {
+  return values;
+}); 
 }
 
 export {
