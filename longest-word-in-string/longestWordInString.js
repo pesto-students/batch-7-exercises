ff73202f9669d224b450cf7aca3ae25cb19b5658
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-06 06:16:12 +0530

    Solve longestWordInString

--- longest-word-in-string/longestWordInString.js
+++ longest-word-in-string/longestWordInString.js
@@ -1,8 +1,11 @@
-
-function longestWordInString(...args) {
-  return args;
+function longestWordInString(str) {
+  const words = str.split(' ');
+  return words.reduce((longest, word) => {
+    if (word.length > longest) {
+      return word.length;
+    }
+    return longest;
+  }, 0);
 }
 
-export {
-  longestWordInString,
-};
+export { longestWordInString };
