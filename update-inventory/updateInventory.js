17b3965ec9cd97f5e5bd4978c6b3a836e5e6c4f6
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-06 08:27:33 +0530

    Solve updateInventory

--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,8 +1,23 @@
-
-function updateInventory(...args) {
-  return args;
+function updateInventory(currentInventory, newInventory) {
+  const newItemsToAdd = [];
+  newInventory.forEach((item) => {
+    const elementIndex = currentInventory.findIndex(currItem => currItem[1] === item[1]);
+    if (elementIndex < 0) {
+      newItemsToAdd.push(item);
+    } else {
+      currentInventory[elementIndex][0] += item[0];
+    }
+  });
+  const updatedInventory = currentInventory.concat(newItemsToAdd);
+  return updatedInventory.sort((item1, item2) => {
+    if (item1[1] < item2[1]) {
+      return -1;
+    }
+    if (item1[1] > item2[1]) {
+      return 1;
+    }
+    return 0;
+  });
 }
 
-export {
-  updateInventory,
-};
+export { updateInventory };
