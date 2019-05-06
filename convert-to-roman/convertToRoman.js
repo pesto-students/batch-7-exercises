8e0ac80cedc283d42859eee23aaf4bff75fadf2c
VikalpP <vikalppanseriya@gmail.com>
2019-05-06 20:59:39 +0530

    Solve convertToRoman

--- convert-to-roman/convertToRoman.js
+++ convert-to-roman/convertToRoman.js
@@ -1,8 +1,28 @@
+function convertToRoman(num) {
+  const fixedRomanSymbols = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
 
-function convertToRoman(...args) {
-  return args;
+  let numberInEnglish = num;
+  let numberInRoman = '';
+  for (const i of Object.keys(fixedRomanSymbols)) {
+    const q = Math.floor(numberInEnglish / fixedRomanSymbols[i]);
+    numberInEnglish -= q * fixedRomanSymbols[i];
+    numberInRoman += i.repeat(q);
+  }
+  return numberInRoman;
 }
 
-export {
-  convertToRoman,
-};
+export { convertToRoman };
