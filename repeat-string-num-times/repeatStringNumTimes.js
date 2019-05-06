2331cb7c69f54e313a5ff5c17810ed4ca2fc31e8
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-06 06:41:19 +0530

    Solve repeatStringNumTimes

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,8 +1,14 @@
-
-function repeatStringNumTimes(...args) {
-  return args;
+function repeatStringNumTimes(str, repeatBy) {
+  let repeatedStr = '';
+  if (repeatBy < 1) {
+    return repeatedStr;
+  }
+  let count = 0;
+  while (count < repeatBy) {
+    repeatedStr += str;
+    count += 1;
+  }
+  return repeatedStr;
 }
 
-export {
-  repeatStringNumTimes,
-};
+export { repeatStringNumTimes };
