91c95f5954c14e9bfb5b8018ab75ac7aa080150c
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-06 18:39:48 +0530

    Solve memoize and request validator

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,12 @@
 
-function memoize(...args) {
-  return args;
+function memoize(fn) {
+  let cache = {};
+  return function(...num) {
+    if (num in cache) {
+      return cache[num];
+    }
+    return cache[num] = fn.apply(this,num);
+  }
 }
 
 export {
--- request-validator/requestValidator.js
+++ request-validator/requestValidator.js
@@ -1,6 +1,32 @@
+function isValidHTTPMethod(httpRequestObject) {
+  const validMethods = ['GET','PUT','POST','DELETE','PATCH'];
+  return validMethods.includes(httpRequestObject.method);
+}
 
-function requestValidator(...args) {
-  return args;
+
+function isValidUri(httpRequestObject) {
+  return /^(https?:\/\/)?([a-z\d]([a-z\d-.]?[a-z\d])*\.[a-z]([a-z\.]?[a-z])*){2,255}(\/[\w-\.~!$&'()*+,;=:@%]*)*$/i.test(httpRequestObject.uri);
+}
+
+function isMessagePresent(httpRequestObject) {
+  return httpRequestObject.hasOwnProperty('message');
+}
+
+
+function requestValidator(httpRequestObject) {
+
+  let errorMessage = 'Invalid request header:';
+  if (!isValidHTTPMethod(httpRequestObject)) {
+    throw new Error(`${errorMessage} Invalid Method`);
+  } 
+  if (!isMessagePresent(httpRequestObject)) {
+    throw new Error(`${errorMessage} Invalid Message`);
+  }
+  if (!isValidUri(httpRequestObject)) {
+    throw new Error(`${errorMessage} Invalid URI`);
+  }
+
+  return httpRequestObject;
 }
 
 export {
