3b21f86e74ae661f5baacd2fe5627ef6836c959a
amitbadala <amitbadala07@gmail.com>
2019-05-07 02:24:44 +0530

    create-student

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,15 +1,20 @@
 
 function createStudent(obj) {
+  debugger
   let student = {};
-  student.likesES2015 =  false;
-  student.likesJavaScript = false; 
-
-  let comleteStatement = '';
-  comleteStatement = (!student.likesES2015 && !student.likesJavaScript)?'does not like much...':'likes ';
-
-  return `The student ${comleteStatement} ${student.likesJavaScript?'JavaScript':''}
-  ${(student.likesJavaScript && student.likesES2015)?' and ':''}
-  ${student.likesES2015?'ES2015':''}`;
+  if(obj !== undefined){
+    student.likesES2015 =  obj.hasOwnProperty('likesES2015')?obj.likesES2015:true;
+    student.likesJavaScript = obj.hasOwnProperty('likesJavaScript')?obj.likesJavaScript:true;   
+    if((!student.likesES2015 && !student.likesJavaScript)){
+      return 'The student does not like much...';
+    }
+    else{
+      return `The student likes ${student.likesJavaScript?'JavaScript':''}${(student.likesJavaScript && student.likesES2015)?' and ':''}${student.likesES2015?'ES2015':''}!`; 
+    }  
+  }
+  else{
+    return 'The student likes JavaScript and ES2015';
+  }
 }
 
 export {
