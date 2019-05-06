c282e6aceddcb980f5ec5e5a34f65306693f13e2
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-06 18:58:46 +0530

    Solve truth-check

--- truth-check/truthCheck.js
+++ truth-check/truthCheck.js
@@ -1,6 +1,12 @@
 
-function truthCheck(...args) {
-  return args;
+function truthCheck(input, predicate) {
+  debugger;
+  input.forEach(object => {
+    if (!(object.hasOwnProperty(predicate) && Boolean(object[predicate]))) {
+      return false;
+    }
+  });
+  return true;
 }
 
 export {
