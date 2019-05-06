154c4f05911f7a84c76007c620ce407c6bf9dc59
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-06 08:35:56 +0530

    Solve smallestCommons

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,8 +1,25 @@
-
-function smallestCommons(...args) {
-  return args;
+function HCF(num1, num2) {
+  return !num2 ? num1 : HCF(num2, num1 % num2);
 }
 
-export {
-  smallestCommons,
-};
+function LCM(num1, num2) {
+  return (num1 * num2) / HCF(num1, num2);
+}
+
+function smallestCommons(range) {
+  let lowerBound = range[0];
+  let upperBound = range[1];
+  if (range[0] > range[1]) {
+    lowerBound = range[1];
+    upperBound = range[0];
+  }
+  let smallestCommon = lowerBound;
+
+  for (let i = lowerBound; i <= upperBound; i += 1) {
+    smallestCommon = LCM(smallestCommon, i);
+  }
+
+  return smallestCommon;
+}
+
+export { smallestCommons };
