f997ebc5d8fa85f278efd610f7b4078cb45c394d
Pratiyush <pkpratiyush@gmail.com>
2019-05-08 05:22:10 +0530

    Solving exercises Day-7 segment-2

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,4 @@
 
-[WIP]:
-
 function animalSort(arr) {
     if (arr.length === 0) {
       return [];
@@ -15,6 +13,12 @@ function animalSort(arr) {
           return -1;
         }
       }
+      else if (difference < 0) {
+        return -1;
+      }
+      else {
+        return 1;
+      }
     });
     console.log(arr);
 
@@ -22,12 +26,12 @@ function animalSort(arr) {
   
 
 
-// export {
-//   animalSort,
-// };
-const arr = [
-  { name: 'Dog', numberOfLegs: 4 },
-  { name: 'Bird', numberOfLegs: 2 },
-  { name: 'Snake', numberOfLegs: 0 },
-];
- animalSort(arr);
+export {
+  animalSort,
+};
+// const arr = [
+//   { name: 'Dog', numberOfLegs: 4 },
+//   { name: 'Bird', numberOfLegs: 2 },
+//   { name: 'Snake', numberOfLegs: 0 },
+// ];
+//  animalSort(arr);
