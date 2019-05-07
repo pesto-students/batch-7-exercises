95e558f8b7843a0d5c1c4ae304b39f8a103533ae
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-08 05:03:39 +0530

    Solve animal-sort aperture

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,21 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(array) {
+  if (array.length === 0) {
+    return array;
+  }
+  array.sort((animalOne,animalTwo) => {
+    if(animalOne.numberOfLegs < animalTwo.numberOfLegs) {
+      return -1;
+    }
+    if (animalOne.numberOfLegs > animalTwo.numberOfLegs) {
+      return 1;
+    }
+    if (animalOne.numberOfLegs === animalTwo.numberOfLegs && animalOne.name < animalTwo.name) {
+      return -1;
+    }
+    return 1;
+  });
+  return array;
 }
 
 export {
--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,15 @@
 
-function aperture(...args) {
-  return args;
+function aperture(n,array) {
+  if (n > array.length) {
+    return [];
+  } 
+  let count = 0;
+  const transformedArray = [];
+  while (count < array.length - n + 1)  { // Not going beyond length - size of each tuple
+    transformedArray.push(array.slice(count, count + n));
+    count += 1;
+  }
+  return transformedArray;
 }
 
 export {
