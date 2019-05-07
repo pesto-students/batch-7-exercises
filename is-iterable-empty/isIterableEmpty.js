6227f418f5cea16c0f6f338f97301cbba668d802
VikalpP <vikalppanseriya@gmail.com>
2019-05-08 00:18:17 +0530

    Solve isIterableEmpty

--- is-iterable-empty/isIterableEmpty.js
+++ is-iterable-empty/isIterableEmpty.js
@@ -1,8 +1,11 @@
+import { isIterable } from '../is-iterable/isIterable';
 
-function isIterableEmpty(...args) {
-  return args;
+function isIterableEmpty(iterable) {
+  if (!isIterable(iterable)) return false;
+
+  const iterator = iterable[Symbol.iterator]();
+  const { done } = iterator.next();
+  return done;
 }
 
-export {
-  isIterableEmpty,
-};
+export { isIterableEmpty };
