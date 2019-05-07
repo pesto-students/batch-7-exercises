72553adf0b92dc3c3e327aee437d111fb99f8717
manish zanzad <manishzanzad@gmail.com>
2019-05-08 00:55:35 +0530

    Working in progress exercise Cycled

--- cycled/Cycled.js
+++ cycled/Cycled.js
@@ -1,8 +1,47 @@
+class Cycled {
+  constructor(_array) {
+    this.array = _array;
+    this.index = 0;
+    this.isReversed = false;
+  }
 
-function cycled(...args) {
-  return args;
+  set index(value) {
+    if (value === -1) {
+      this.index = this.array.length - 1;
+    } else {
+      this.index = (value % this.array.length);
+    }
+  }
+
+  [Symbol.iterator]() {
+    return this;
+  }
+  next() {
+    if (this.isReversed) {
+      this.index++;
+      return this.array[this.index];
+    }
+    else {
+      previous();
+    }
+  }
+  current() {
+    return this.array[this.index];
+  }
+  previous() {
+    if (this.isReversed) {
+      this.index--;
+      return this.array[this.index];
+    }
+    else {
+      next();
+    }
+  }
+  reverse() {
+    this.isReversed = !this.isReversed;
+  }
 }
 
 export {
-  cycled,
+  Cycled,
 };
