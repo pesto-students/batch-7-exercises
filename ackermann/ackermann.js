1f0cbfb874713ccd4ea959921d1e83a0362c9839
Pratiyush <pkpratiyush@gmail.com>
2019-05-08 05:12:46 +0530

    Solving exercises Day-7 segment-2

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,7 +1,13 @@
+function ackerman(m, n) {
+  return m === 0 ? n + 1 : ackerman(m - 1, n === 0  ? 1 : ackerman(m, n - 1));
+ }
+function ackermann(obj) {
+  const { m, n } = obj;
 
-function ackermann(...args) {
-  return args;
+const result = ackerman (m, n);
+return result;
 }
+
 
 export {
   ackermann,
