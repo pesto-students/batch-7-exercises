016a3c929a0e3404f2d878cf1a072aa7548b7243
VikalpP <vikalppanseriya@gmail.com>
2019-05-07 09:27:38 +0530

    Solve Queue

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,32 @@
+class Queue {
+  constructor() {
+    this.data = [];
+  }
 
-function queue(...args) {
-  return args;
+  isEmpty() {
+    return this.data.length === 0;
+  }
+
+  enqueue(data) {
+    this.data.push(data);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return null;
+    return this.data.shift();
+  }
+
+  peek() {
+    if (this.isEmpty()) return null;
+    return this.data[0];
+  }
+
+  toString(stringifier = null) {
+    if (stringifier) {
+      return this.data.map(stringifier).join(',');
+    }
+    return this.data.toString();
+  }
 }
 
-export {
-  queue,
-};
+export { Queue };
