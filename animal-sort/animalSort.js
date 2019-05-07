ee8f95872c0460f63707bc8f8f006e829dd41f27
amitbadala <amitbadala07@gmail.com>
2019-05-08 05:13:19 +0530

    solve animalsort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,17 +1,17 @@
 
 function sortByName(a,b){
-  var A = a.name.toUpperCase();
-  var B = b.name.toUpperCase();
-  if (A < B) {
+  var animalA = a.name.toUpperCase();
+  var animalB = b.name.toUpperCase();
+  if (animalA < animalB) {
     return -1;
   }
-  if (A < B) {
+  if (animalA < animalB) {
     return 1;
   } 
   return 0; 
 }
 function animalSort(animals) {
-  return animals.sort((a, b) => a.numberOfLegs - b.numberOfLegs)
+  return animals.sort((a, b) => a.numberOfLegs - b.numberOfLegs && sortByName(a,b))
 }
 
 export {
