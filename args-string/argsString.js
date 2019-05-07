2de21a542389ce9b81d71acec4cb09b3e47c8279
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-08 05:09:28 +0530

    Solve argsString

--- args-string/argsString.js
+++ args-string/argsString.js
@@ -1,8 +1,16 @@
-
-function argsString(...args) {
-  return args;
+function argsString(str, params) {
+  let modifiedString = '';
+  let paramIndex = 0;
+  for (let char of str) {
+    if (char === '{') {
+      char = params[paramIndex];
+      paramIndex += 1;
+    } else if (char === '}') {
+      char = '';
+    }
+    modifiedString += char;
+  }
+  return modifiedString;
 }
 
-export {
-  argsString,
-};
+export { argsString };
