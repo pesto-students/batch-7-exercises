18f40652bcf3acda4d4e9e9688a3a55894496230
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-09 00:30:58 +0530

    Solve knownProp

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,8 +1,13 @@
-
-function knownProp(...args) {
-  return args;
+function knownProp(obj) {
+  const proxyHandler = {
+    get(target, property) {
+      if (property in target) {
+        return target[property];
+      }
+      throw new TypeError('Unknown property.');
+    },
+  };
+  return new Proxy(obj, proxyHandler);
 }
 
-export {
-  knownProp,
-};
+export { knownProp };
