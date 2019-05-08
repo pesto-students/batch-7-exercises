d2b8bec2d5953f2c32b46bb3e3ae5bd4e3f6b621
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-09 00:58:33 +0530

    Solved known Prop and Solving negative-index

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,8 +1,14 @@
-
-function knownProp(...args) {
-  return args;
+function knownProp(object) {
+  const handler = {
+    get: (object, property) => {
+      if (property in object) {
+        return object[property];
+      } else {
+        throw new TypeError("Unknown property");
+      }
+    }
+  };
+  return new Proxy(object, handler);
 }
 
-export {
-  knownProp,
-};
+export { knownProp };
--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,7 +1,20 @@
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(array) {
+  if (array.constructor !== Array) {
+    return new TypeError('Only arrays are supported')
+  }
+  const handler =  {
+    get: (array, index) => {
+      if (index >= 0 ) {
+        return array[index];
+      } else {
+        return array[array.length + index];  //  -(-index) is equeal +index
+      }
+    }
+  }
+  return new Proxy(array, handler);
 }
+  
 
 export {
   negativeIndex,
