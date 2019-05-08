347d07df42948ab425cfd72eb7f2be44edcd649e
suhadev <suhadevvenkatesh@gmail.com>
2019-05-09 05:25:43 +0530

    Solve slice,splitEvery

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,6 +1,15 @@
 
-function splitEvery(...args) {
-  return args;
+function splitEvery(noOfCharactersToExtract,arr) {
+    let splittedArray = []
+    let slicedArray = []
+    if(arr.length === 0) {
+      throw new Error("Expected some value but input array is null")
+    }
+    for(let i = 0; i < arr.length;i = i + (noOfCharactersToExtract)) {
+       slicedArray = arr.slice(i,noOfCharactersToExtract + i)
+       splittedArray.push(slicedArray)
+    }
+    return splittedArray
 }
 
 export {
