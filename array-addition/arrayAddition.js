4f583d7450e96fd7cce24850ac85a36bf49cb539
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-08 06:26:27 +0530

    Solve arrayAddition

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,8 +1,30 @@
+const isNumber = num => typeof num === 'number';
 
-function arrayAddition(...args) {
-  return args;
+function addArrays(array1, array2) {
+  let index = 0;
+  const summationArray = array1.map((el) => {
+    const secondArrayEl = array2[index];
+    if (!isNumber(el)) {
+      throw new Error(`Expected numerical values. Received ${typeof el}.`);
+    }
+    if (!isNumber(secondArrayEl)) {
+      throw new Error(`Expected numerical values. Received ${typeof secondArrayEl}.`);
+    }
+    index += 1;
+    return el + secondArrayEl;
+  });
+  const extraElements = array2.slice(index);
+  return summationArray.concat(extraElements);
 }
 
-export {
-  arrayAddition,
-};
+function arrayAddition(array1, array2) {
+  if (array1.length === 0 || array2.length === 0) {
+    throw new Error('Received empty array.');
+  }
+  if (array1.length <= array2.length) {
+    return addArrays(array1, array2);
+  }
+  return addArrays(array2, array1);
+}
+
+export { arrayAddition };
