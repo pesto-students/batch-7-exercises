8098e77b8e5c6f9ce19a6b11999a9ca88b3d346d
Pratiyush <pkpratiyush@gmail.com>
2019-05-09 03:27:02 +0530

    solving exercises day-8 segment-1

--- without/without.js
+++ without/without.js
@@ -1,8 +1,11 @@
 
-function without(...args) {
-  return args;
+function without(valuesToBeDeleted, array) {
+  const map = new Map();
+  map = array;
+  console.log (map.forEach(array));
 }
 
-export {
-  without,
-};
+// export {
+//   without,
+// };
+without([1, 2], [1, 2, 1, 4, 5]);
