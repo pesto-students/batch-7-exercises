97552e15ab2238a2235f726e03d25ac916298a46
suhadev <suhadevvenkatesh@gmail.com>
2019-05-08 21:34:35 +0530

    Solve rangeIter,generator iterable

--- generator-iterable/generatorIterable.js
+++ generator-iterable/generatorIterable.js
@@ -1,16 +1,7 @@
 
-function generatorIterable(...args) {
-  generatorIterable[Symbol.iterator] = function() {
-    let i = 0
-    next : function() {
-        i++
-        if(i <= 5) {
-          return{ value : i,done : false}
-        }
-        else {
-          return{ done : true}
-        }
-    }
+function* generatorIterable() {
+  for(let i = 1; i <= 5; i++){
+    yield i
   }
 }
 
