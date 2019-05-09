4a768549871a73c906cd5a09abfb8f92af2e9b5f
Pratiyush <pkpratiyush@gmail.com>
2019-05-10 01:30:49 +0530

    Solving Exercises Day-9-Segment-1

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -1,8 +1,31 @@
 
-function binarySearch(...args) {
-  return args;
+function binarySearch(array, numberToSearch) {
+  let lowerBound = 0;
+  let upperBound = array.length - 1;
+  let middleOfArray;
+  let valueFound = false;
+  while (lowerBound < upperBound) {
+    middleOfArray = (lowerBound + upperBound) / 2;
+    middleOfArray = Math.floor(middleOfArray);
+    console.log(middleOfArray);
+    if (array[middleOfArray] < numberToSearch) {
+      lowerBound = middleOfArray + 1;
+    }
+    else if (array[middleOfArray] > numberToSearch) {
+      upperBound = middleOfArray - 1;
+    }
+    else {
+      valueFound = true;
+      return middleOfArray;
+     
+    }
+  }
+  if (valueFound === false) {
+    return -1;
+  }
 }
 
-export {
-  binarySearch,
-};
+// export {
+//   binarySearch,
+// };
+binarySearch([1, 2], 1);
