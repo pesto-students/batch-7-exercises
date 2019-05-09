540d95c7413b2a194f538908d4f47abe007f08c4
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-09 06:18:44 +0530

    Solve slice

--- slice/slice.js
+++ slice/slice.js
@@ -1,8 +1,14 @@
-
-function slice(...args) {
-  return args;
+function slice(startIndex, endIndex, object) {
+  let array = object;
+  const isString = typeof object === 'string';
+  if (isString) {
+    array = object.split('');
+  }
+  const slicedArray = array.slice(startIndex, endIndex);
+  if (isString) {
+    return slicedArray.join('');
+  }
+  return slicedArray;
 }
 
-export {
-  slice,
-};
+export { slice };
