733b8bfe0ab37a77a44582ecd42caef7b308160c
VikalpP <vikalppanseriya@gmail.com>
2019-05-09 06:12:24 +0530

    Solve searchSortedMatrx

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,8 +1,25 @@
+function searchSortedMatrix({ search, matrix }) {
+  let rowToSearchIn = 0;
 
-function searchSortedMatrix(...args) {
-  return args;
+  for (let rowIndex = 0; rowIndex < matrix.length; rowIndex += 1) {
+    const row = matrix[rowIndex];
+    const firstValueOfRow = row[0];
+
+    if (search < firstValueOfRow) {
+      rowToSearchIn = rowIndex - 1;
+      break;
+    }
+  }
+
+  const rowToLookInto = matrix[rowToSearchIn];
+  let columnLength = rowToLookInto.length;
+  while (columnLength) {
+    columnLength -= 1;
+    if (search === rowToLookInto[columnLength]) {
+      return true;
+    }
+  }
+  return false;
 }
 
-export {
-  searchSortedMatrix,
-};
+export { searchSortedMatrix };
