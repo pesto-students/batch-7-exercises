1ce7cd56c6ab2624e83e94c22c9c4cfb8247a21b
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-09 06:35:55 +0530

    Solve splitEvery

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,8 +1,20 @@
-
-function splitEvery(...args) {
-  return args;
+function splitEvery(splitIn, array) {
+  if (splitIn < 1) {
+    throw new Error(`Only positive numbers are expected. Received ${splitIn}`);
+  }
+  const isString = typeof array === 'string';
+  if (isString) {
+    array = array.split('');
+  }
+  const splitElements = [];
+  while (array.length > 0) {
+    let splitArray = array.splice(0, splitIn);
+    if (isString) {
+      splitArray = splitArray.join('');
+    }
+    splitElements.push(splitArray);
+  }
+  return splitElements;
 }
 
-export {
-  splitEvery,
-};
+export { splitEvery };
