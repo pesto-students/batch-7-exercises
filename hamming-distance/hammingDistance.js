a5b9b67528f5d4fdf3a7ebbc9699fe89c31632fb
VikalpP <vikalppanseriya@gmail.com>
2019-05-10 01:51:48 +0530

    Solve hammingDistance

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,8 +1,19 @@
+function hammingDistance(firstString, secondString) {
+  const lengthOfFirstString = firstString.length;
+  const lengthOfSecondString = secondString.length;
 
-function hammingDistance(...args) {
-  return args;
+  if (lengthOfFirstString !== lengthOfSecondString) {
+    throw new Error(`Expected two same length strings, Received string lengths ${lengthOfFirstString}, ${lengthOfSecondString}`);
+  }
+
+  return secondString
+    .split('')
+    .reduce((acc, char, index) => {
+      const charOfFirstStringAtIndex = firstString.charAt(index);
+      return (char !== charOfFirstStringAtIndex)
+        ? acc + 1
+        : acc;
+    }, 0);
 }
 
-export {
-  hammingDistance,
-};
+export { hammingDistance };
