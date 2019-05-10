b340323c4bc96daf39c57e6bdb20d9696ee31c91
Pratiyush <pkpratiyush@gmail.com>
2019-05-10 08:17:22 +0530

    Solving Day-9 Segment-1 exercises

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,6 +1,16 @@
 
-function hammingDistance(...args) {
-  return args;
+function hammingDistance(str1, str2) {
+  if ( str1.length !== str2.length) {
+    throw new Error('Cannot match strings of different length');
+  }
+  let count = 0;
+  for ( let i = 0;i < str1.length; i ++) {
+    if (str1[i] !== str2[i]) {
+      count ++;
+    }
+  }
+  return count;
+
 }
 
 export {
