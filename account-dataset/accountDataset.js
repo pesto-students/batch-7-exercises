a5275a601e744e68b81c4fdee551ef01a44615dc
amitbadala <amitbadala07@gmail.com>
2019-05-12 03:27:52 +0530

    Solve account-dataset

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,54 +1,66 @@
-const dataset = require('./dataset.json');
- 
-function datasetWithRoundedDollar()
-{
-  dataset.bankBalances.forEach((el)=>{
-     el.rounded = Math.round(el.amount);
+const dataset = require("./dataset.json");
+
+function datasetWithRoundedDollar() {
+  dataset.bankBalances.forEach(el => {
+    el.rounded = Math.round(el.amount);
   });
   return dataset.bankBalances;
 }
 
-function hundredThousandairs(){
-  var accountsResult = dataset.bankBalances.filter((account)=>{
+function hundredThousandairs() {
+  var accountsResult = dataset.bankBalances.filter(account => {
     return account.amount > 100000;
   });
   return accountsResult;
 }
 
-function sumOfBankBalances(){
-  var totalAmount = 0;
-  function sumofAmount(sum, num) { 
-    return sum.amount + num.amount; 
+function sumOfBankBalances() {
+  let accounts = dataset.bankBalances;
+  let sumAmount = accounts.reduce((sum, account) => {
+    return sum + parseFloat(account.amount);
+  }, 0);
+  return parseFloat(sumAmount.toFixed(2));
+}
+
+function sumOfInterests() {
+  let stateCodes = ["WI", "IL", "WY", "OH", "GA", "DE"];
+  const interestRate = 18.9;
+
+  let filterStateWise = dataset.bankBalances.filter(account => {
+    return stateCodes.indexOf(account.state) != -1;
+  });
+  let payableAmount = filterStateWise.reduce((sum, account) => {
+    return parseFloat(
+      (sum + (parseFloat(account.amount) * interestRate) / 100).toFixed(2)
+    );
+  }, 0);
+
+  return payableAmount;
+}
+function higherStateSums() {
+  let totalAmountStateWise = {};
+  let sumAmountThreshold = 1000000;
+  let amountSum = 0;
+  dataset.bankBalances.forEach(account => {
+    if (totalAmountStateWise[account.state]) {
+      totalAmountStateWise[account.state] += parseFloat(account.amount);
+    } else {
+      totalAmountStateWise[account.state] = parseFloat(account.amount);
+    }
+  });
+
+  for (var key in totalAmountStateWise) {
+    if (totalAmountStateWise[key] > sumAmountThreshold)
+      amountSum += totalAmountStateWise[key];
   }
-  totalAmount = dataset.bankBalances.reduce(sumOfAmount); 
-  return Math.round(totalAmount);
-}
 
-function sumOfInterests(){ 
-  var stateCodes = ["WI","IL","WY","OH","GA","DE"];
-  var interestPayable = 0;
-  function sumofAmount(sum, num) { 
-    return sum.amount + num.amount; 
-  }
-  var filterStateWise = dataset.bankBalances.filter((account)=>{
-    return  stateCodes.indexOf(account.state) == 1;
-  })
-  totalAmount = filterStateWise.reduce(sumOfAmount); 
-  interestPayable = calculateInterest(18.9,Math.round(totalAmount));
-  return interestPayable
+  return amountSum;
 }
-function higherStateSums(){
-
-}
-
-function calculateInterest(interestRate,amount)
-{
-  var interestPayable =  (amount*interestRate*1)/100;
-  return interestPayable;
-}
- 
 
 export {
-  datasetWithRoundedDollar,hundredThousandairs,sumOfBankBalances,sumOfInterests,
+  datasetWithRoundedDollar,
+  hundredThousandairs,
+  sumOfBankBalances,
+  sumOfInterests,
   higherStateSums
 };
