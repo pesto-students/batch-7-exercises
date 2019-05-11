07fe27c9b279c0ef0f0cb53608c1165ece8cdbf1
VikalpP <vikalppanseriya@gmail.com>
2019-05-12 00:32:00 +0530

    Solve updateInventory

--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,8 +1,22 @@
+const getCounts = item => item[0];
+const getName = item => item[1];
 
-function updateInventory(...args) {
-  return args;
+const compareAlphabetically = (item1, item2) => getName(item1).localeCompare(getName(item2));
+
+function updateInventory(currentInventory, newInventory) {
+  const finalInventory = currentInventory.reduce((acc, item) => {
+    const newItemIndex = newInventory.findIndex(newItem => getName(newItem) === getName(item));
+    if (newItemIndex === -1) {
+      acc.push(item);
+    } else {
+      const prevItem = acc[newItemIndex];
+      const totalItemCount = getCounts(prevItem) + getCounts(item);
+      acc[newItemIndex] = [totalItemCount, getName(item)];
+    }
+    return acc;
+  }, newInventory);
+
+  return finalInventory.sort(compareAlphabetically);
 }
 
-export {
-  updateInventory,
-};
+export { updateInventory };
