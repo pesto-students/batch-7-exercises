f851b3772f1c89484739fc5dbe22716e8605ef91
VikalpP <vikalppanseriya@gmail.com>
2019-05-11 22:15:51 +0530

    Solve largeOfFour

--- large-of-four/largeOfFour.js
+++ large-of-four/largeOfFour.js
@@ -1,8 +1,8 @@
-
-function largeOfFour(...args) {
-  return args;
+function largeOfFour(arrays) {
+  return arrays.reduce((acc, array) => {
+    acc.push(Math.max(...array));
+    return acc;
+  }, []);
 }
 
-export {
-  largeOfFour,
-};
+export { largeOfFour };
