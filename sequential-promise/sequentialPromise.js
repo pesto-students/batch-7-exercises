af76e7e3a1037d59b63bff776c38208757b88718
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-12 08:20:58 +0530

    Solve sequentialPromise

--- sequential-promise/sequentialPromise.js
+++ sequential-promise/sequentialPromise.js
@@ -1,8 +1,12 @@
-
-function sequentialPromise(...args) {
-  return args;
+function sequentialPromise(fns, resolvedValue) {
+  if (fns.length === 1) {
+    return Promise.resolve(fns[0](resolvedValue));
+  }
+  return new Promise((resolve, reject) => {
+    fns[0](resolvedValue)
+      .then(res => resolve(sequentialPromise(fns.slice(1), res)))
+      .catch(e => reject(e));
+  });
 }
 
-export {
-  sequentialPromise,
-};
+export { sequentialPromise };
