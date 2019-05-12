579dc9c1b9779ffc54f75fbcb1441ff1a11071ef
amitbadala <amitbadala07@gmail.com>
2019-05-12 06:17:01 +0530

    wip solve memoize function

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,20 +1,14 @@
-
-const memoize = (fn,unused) => {
-  debugger;
+const memoize = fun => {
   let cache = {};
-  return (...args) => {
-    let n = args[0];
-    if (n in cache) { 
+  return function(n) {
+    if (cache[n] != undefined) {
       return cache[n];
-    } 
-    else { 
-      let result = fn(n);
-      cache[n] = result; 
+    } else {
+      let result = fun(n);
+      cache[n] = result;
       return result;
     }
-  }
-}
-
-export {
-  memoize,
+  };
 };
+
+export { memoize };
