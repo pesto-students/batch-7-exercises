b1b420c0449dd00859c6686c8611d8b0db166cf2
suhadev <suhadevvenkatesh@gmail.com>
2019-05-13 02:16:33 +0530

    Solve arrayAddition

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,17 +1,26 @@
 
 function arrayAddition(arr1,arr2) {
-  function isValid(){
-    if(arr1.length === 0 || arr2.length === 0 || typeof arr1 || typeof arr2 !== "object") {
-      return false
-    }
-    else {
+
+    if(arr1.length === 0 || arr2.length === 0 || typeof arr1 !== "object" || typeof arr2 !== "object") {
       throw new Error("Invalid Input")
     }
-  }
-
-  const addedArray = arr1.map((number,index)=> {
-    number + arr2[index]
-  })
+    let addedArray = []
+    if(arr1.length > arr2.length || arr1.length === arr2.length){
+          addedArray = arr1.map((number,index)=> {
+           if(typeof number !== "number" || typeof arr2[index] !== "number"){
+            throw new Error("Invalid Input")
+           }
+          return number + arr2[index]
+         })
+     }
+     if(arr2.length > arr1.length){
+      addedArray = arr2.map((number,index)=> {
+       if(typeof number !== "number" || typeof arr1[index] !== "number"){
+        throw new Error("Invalid Input")
+       }
+      return number + arr1[index]
+     })
+ }
   return addedArray
 }
 
