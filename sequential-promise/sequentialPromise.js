f9f683072a6eae4d587f9117522c48c64a3a4604
amitbadala <amitbadala07@gmail.com>
2019-05-12 23:04:51 +0530

    Revert "Solve sequential-promise"
    This reverts commit 94ed0968244d25fcc294ebde23f0605e9b2ca75f.
    
    Committed test file by mistake

--- sequential-promise/sequentialPromise.js
+++ sequential-promise/sequentialPromise.js
@@ -1,9 +1,17 @@
-function sequentialPromise(promiseChain) {
-  let result = promiseChain.reduce(
-    (acc, currentFunction) => acc.then(currentFunction),
-    Promise.resolve(1)
-  );
-  return result;
+
+function sequentialPromise(promise1,promise2,promise3) {  
+  // return args[0]().then((val)=>args[1](val).then((d)=>args[2](d).then((v)=> {
+  //   return v
+  // });
+function sumofArray(sum, num) { 
+    return sum + num; 
+}  
+
+Promise.all([promise1, promise2, promise3]).then(function(values) {
+  return values,reduce(sumofArray);
+}); 
 }
 
-export { sequentialPromise };
+export {
+  sequentialPromise,
+};
