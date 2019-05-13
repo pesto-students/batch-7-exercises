0b27abfe86a316f74283e2fc8254f240d1e9a9db
suhadev <suhadevvenkatesh@gmail.com>
2019-05-13 17:16:34 +0530

    Solve knownProp,setDefaultProp

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,15 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp(obj) {
+  const handler = {
+    get : function(obj,prop) {
+      if(!obj.hasOwnProperty(prop)) {
+        throw TypeError("Unkown Property")
+      }
+      return obj[prop]
+    }
+  }
+  const proxy = new Proxy(obj,handler)
+  return proxy
 }
 
 export {
--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,20 @@
+import { isArray } from "util";
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(obj) {
+  const handler = {
+    get : function(obj,index) {
+      if(Array.isArray(obj) === "false") {
+        throw TypeError("Only arrays are supported")
+      }
+      if(index < 0) {
+        return obj[obj.length - 2]
+      }
+      return obj[index]
+    }
+
+  }
+  const proxy = new Proxy(obj,handler)
+  return proxy
 }
 
 export {
