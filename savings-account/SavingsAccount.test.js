beb6fc3c54138b2c0ec8f0455ca3490cf38692e2
VikalpP <vikalppanseriya@gmail.com>
2019-05-13 08:19:52 +0530

    Solve SavingAccount

--- savings-account/SavingsAccount.test.js
+++ savings-account/SavingsAccount.test.js
@@ -17,15 +17,15 @@ describe('SavingsAccount', () => {
     expect(() => new SavingsAccount('123', 'kohli@gmail.com', 'Virat', 'Kohli')).toThrow('Account Number must be a 6-digit number');
   });
 
-  test('should throw error if account number length is not equal to 6', () => {
+  test('should throw error if email address format is invalid', () => {
     expect(() => new SavingsAccount('123456', 'kohli@', 'Virat', 'Kohli')).toThrow('Invalid e-mail');
   });
 
-  test('should throw error if account number length is not equal to 6', () => {
+  test('should throw error if length of first name is not between 3 and 20 characters.', () => {
     expect(() => new SavingsAccount('123456', 'kohli@gmail.com', 'V', 'Kohli')).toThrow('First name must be between 3 and 20 characters long');
   });
 
-  test('should throw error if account number length is not equal to 6', () => {
+  test('should throw error if last name do not have english alphabets', () => {
     expect(() => new SavingsAccount('123456', 'kohli@gmail.com', 'Virat', 'Kohli5')).toThrow('Last name must contain english alphabets only');
   });
 });
