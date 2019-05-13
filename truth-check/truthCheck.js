9c6454a5278404a5548e7ffb6559756929c0ebd8
suhadev <suhadevvenkatesh@gmail.com>
2019-05-13 14:21:39 +0530

    Solve userCheckPassword

--- truth-check/truthCheck.js
+++ truth-check/truthCheck.js
@@ -1,6 +1,12 @@
 
-function truthCheck(...args) {
-  return args;
+function truthCheck(array, predicate) {
+  for (let i = 0; i < array.length; i++) {
+    let object = array[i];
+    if (!object[predicate]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export {
