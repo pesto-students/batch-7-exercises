e292b72741745ea590decd1e720ad913573de761
manish zanzad <manishzanzad@gmail.com>
2019-05-13 05:33:35 +0530

    Solve excersice fibonacci-iter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,8 +1,16 @@
-
-function fibonacciIter(...args) {
-  return args;
-}
-
-export {
-  fibonacciIter,
+const fibonacciIter = {
+  [Symbol.iterator]: () => {
+    let previousFact = 0;
+    let currentFact = 1;
+    return {
+      next() {
+        const fact = previousFact + currentFact;
+        previousFact = currentFact;
+        currentFact = fact;
+        return { value: fact, done: false };
+      },
+    };
+  },
 };
+
+export { fibonacciIter };
