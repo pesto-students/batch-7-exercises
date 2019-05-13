0c218110c309e04e2e8f4da12b309710344719ca
amitbadala <amitbadala07@gmail.com>
2019-05-13 20:51:42 +0530

    Solve setDeafultProperty

--- set-default-property/setDefaultProperty.js
+++ set-default-property/setDefaultProperty.js
@@ -1,8 +1,11 @@
-
-function setDefaultProperty(...args) {
-  return args;
+function setDefaultProperty(obj, defaultVal) {
+  const handler = {
+    get: function(target, name) {
+      return target.hasOwnProperty(name) ? target[name] : defaultVal;
+    }
+  };
+  let resultObj = new Proxy(obj, handler);
+  return resultObj;
 }
 
-export {
-  setDefaultProperty,
-};
+export { setDefaultProperty };
