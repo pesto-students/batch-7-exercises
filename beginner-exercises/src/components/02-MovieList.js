58331f18f59462f6cfccb29e6c84d0c53006b7ab
manish zanzad <manishzanzad@gmail.com>
2019-05-15 01:07:29 +0530

    Solving excersice beginner-excersice

--- beginner-exercises/src/components/02-MovieList.js
+++ beginner-exercises/src/components/02-MovieList.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
-
-// We will create dynamic list of movies. Let's see how to render
-// a list of items.
-//
-// Task #1: Add another movie (or movies) to the list.
-// Task #2: Change `ul` HTML attribute `class` to 'movies-list'.
-
+import React, { Component } from "react";
 class MovieList extends Component {
-  // We can put DOM elements delivered by React just like HTML elements.
-  // Doesn't this code look familiar to you?
-  // Refer to App.css for a list a classes or add one yourself
   render() {
     return (
-      <ul className="FILL ME">
+      <ul className="movie-list">
         <li>The Godfather</li>
+        <li>Shawshank Redemption</li>
+        <li>Avenger</li>
+        <li>Chota Bhim</li>
       </ul>
     );
   }
-  // Think about it: Why is that `className` attribute not called just `class`?
-  // Hint: Some words in JavaScript/JSX are reserved...
 }
 
 export default MovieList;
--- beginner-exercises/src/components/03-FavoriteMovie.js
+++ beginner-exercises/src/components/03-FavoriteMovie.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 /*
 You can pass props to components using JSX attributes.
@@ -18,7 +18,7 @@ Further reading on task #2: https://facebook.github.io/react/tips/if-else-in-JSX
 */
 
 class FavoriteMovie extends Component {
-/*
+  /*
   By default `this.state` is `null`. In `render` we are referring to
   a specific element from the `state` object - `this.state.movie`.
   If we don't set an initial state, we will get an error. It's impossible to fetch
@@ -31,8 +31,7 @@ class FavoriteMovie extends Component {
   constructor(props) {
     // Properties object is called `props`. You can access it with `this.props`.
     super(props);
-    this.state = { movie: '' };
-
+    this.state = { movie: "", enteredMovie: false };
     // Warning! If we don't bind this method - we would not be able to update state.
   }
 
@@ -47,15 +46,26 @@ class FavoriteMovie extends Component {
   */
 
   /* eslint-disable no-unused-vars, react/no-unused-state */
-  onMovieChange(event) {
-    // Huh... There's something wrong here...
-    this.setState({ badAttribute: 'ChangeME!' });
-  }
+  onMovieChange = event => {
+    this.state.movie = event.target.value;
+    this.state.enteredMovie = true;
+    if (this.state.movie.length === 0) {
+      this.state.enteredMovie = false;
+      this.state.movie = "Hey there. Enter your favorite movie.";
+    }
+    this.setState({
+      movie: this.state.movie,
+      enteredMovie: this.state.enteredMovie
+    });
+  };
 
   render() {
     return (
       <div>
-        <p>My favorite movie is <span style={{ color: 'blue' }}>{this.state.movie}</span></p>
+        <p>
+          {this.state.enteredMovie ? <label>My favorite movie is</label> : " "}{" "}
+          <span style={{ color: "blue" }}>{this.state.movie}</span>
+        </p>
         <input type="text" name="name" onChange={this.onMovieChange} />
       </div>
     );
