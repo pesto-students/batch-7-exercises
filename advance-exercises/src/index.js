22838fad75a95f4d6e1b6379247489f6e2039c64
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-16 01:10:10 +0530

    Solving compoundComponents

--- advance-exercises/src/index.js
+++ advance-exercises/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Provider from 'react-redux';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux'
+import {creatStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = creatStore(App,{});
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
