15574900f80151d01b3ae3a42e09213a359754b8
amitbadala <amitbadala07@gmail.com>
2019-05-17 00:23:55 +0530

    Solve setupRouter

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,20 +1,26 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+import { Route, Switch } from "react-router-dom";
 
-import './styles/App.css';
+import "./styles/App.css";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Home" component={Home} />
+          <Route path="/About" component={About} />
+          <Route path="/Contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
       </Fragment>
     );
   }
--- setup-router/src/components/Navbar.js
+++ setup-router/src/components/Navbar.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "../components/Home";
+import About from "../components/About";
+import Contact from "../components/Contact";
 
-import '../styles/App.css';
+import "../styles/App.css";
 
 export default class Navbar extends Component {
   render() {
     return (
       <ul className="nav">
-        <li className="active">Home</li>
+        {/* <li className="active">Home</li> 
         <li>About Us</li>
-        <li>Contact</li>
+        <li>Contact</li> */}
+        <Link to="/Home/">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Contact/">Contact</Link>
       </ul>
     );
   }
