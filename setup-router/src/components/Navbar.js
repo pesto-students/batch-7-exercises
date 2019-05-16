2a511540cb1fe553b4fe73f20189517e6ab78b92
manish zanzad <manishzanzad@gmail.com>
2019-05-17 00:08:58 +0530

    Solve excersice setup-router

--- setup-router/src/components/Navbar.js
+++ setup-router/src/components/Navbar.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-
+import { NavLink } from 'react-router-dom';
 import '../styles/App.css';
 
 export default class Navbar extends Component {
   render() {
     return (
       <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
+        <li>
+          <NavLink exact to="/" activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/aboutus" activeClassName="active">
+            About Us
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" activeClassName="active">
+            Contact
+          </NavLink>
+        </li>
       </ul>
     );
   }
---
+++ setup-router/src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NotFound = () => {
+  return <h1>404 Not Found</h1>;
+};
