78c7cb6fe1cd3ddfe4dfbd8b818f8ad9e9eb96ac
Pratiyush <pkpratiyush@gmail.com>
2019-05-17 07:03:45 +0530

    Solving Exercises of day-14-Segment-1

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component, Fragment } from 'react';
 
 import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
 
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Navbar />
-        <Home />
-        <About />
-        <Contact />
-      </Fragment>
+        <Fragment>
+          <Navbar />
+        </Fragment>
+      
     );
   }
 }
--- setup-router/src/components/Navbar.js
+++ setup-router/src/components/Navbar.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import NotMatch from './NotMatch';
+
 
 import '../styles/App.css';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
-      </ul>
+      <BrowserRouter>
+
+        <ul className="nav">
+          <li className="active">
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <Switch>
+        <Route exact path="/" />
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} /> 
+        <Route component={NotMatch} />
+        </Switch>
+        
+      </BrowserRouter>
     );
   }
 }
