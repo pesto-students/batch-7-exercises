c6b260ef152687a594ec291c70f843226871595b
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-19 04:56:47 +0530

    Solve hamming distance

--- hamming-distance/hammingDistance.js
+++ hamming-distance/hammingDistance.js
@@ -1,8 +1,13 @@
+function hammingDistance(stringOne, stringTwo) {
+  if (stringOne.length !== stringTwo.length) {
+    throw new Error("Strings must be of the same length");
+  }
 
-function hammingDistance(...args) {
-  return args;
+  const distance = stringOne.split('').reduce((distance, character, index) => {
+    return character === stringTwo[index] ? distance : distance += 1;
+  }, 0);
+
+  return distance;
 }
 
-export {
-  hammingDistance,
-};
+export { hammingDistance };
