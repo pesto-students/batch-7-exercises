c3b706f2fbc9dbda31077641dbb8dfac4d12e4bf
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-19 05:05:19 +0530

    Solve primarltiy-test

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,6 +1,17 @@
 
-function primalityTest(...args) {
-  return args;
+function primalityTest(number) {
+  if (number < 2) {
+    return false;
+  }
+  const isPrime = num => {
+    for (let i = 2; i <= num / 2; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return isPrime(number);
 }
 
 export {
