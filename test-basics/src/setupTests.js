78e99957aab8cc5d3208174a79485756c29b28a1
amitbadala <amitbadala07@gmail.com>
2019-05-19 00:14:05 +0530

    [wip] Solve testBasics

--- test-basics/src/setupTests.js
+++ test-basics/src/setupTests.js
@@ -1,4 +1,6 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { configure, shallow, mount, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-configure({ adapter: Adapter });
+configure({ adapter: new Adapter() });
+export { shallow, mount, render };
+export default Enzyme;
