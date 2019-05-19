b9068a916817a601d2a54e41bcc6d779f6ede87e
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-19 05:58:12 +0530

    Solve union

--- union/union.js
+++ union/union.js
@@ -1,6 +1,9 @@
 
 function union(list1, list2) {
   const union = new Set([...list1, ...list2]);
+  if (list1.includes(-0) && list2.includes(0) || list2.includes(-0) && list1.includes(0)){
+    union.push(-0);
+  }
   return [...union];
 }
 
