3c8f4cbfc9e1b7d600363ca5b9828c8eab0d1773
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-19 23:53:37 +0530

    Refactored animalsort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -3,14 +3,15 @@ function animalSort(array) {
   if (array.length === 0) {
     return array;
   }
-  array.sort((animalOne,animalTwo) => {
-    if(animalOne.numberOfLegs < animalTwo.numberOfLegs) {
+  array.sort((animalOne, animalTwo) => {
+    const diffInNumberOfLegs = animalOne.numberOfLegs - animalTwo.numberOfLegs;
+    if (diffInNumberOfLegs < 0) {
       return -1;
     }
-    if (animalOne.numberOfLegs > animalTwo.numberOfLegs) {
+    if (diffInNumberOfLegs > 0) {
       return 1;
     }
-    if (animalOne.numberOfLegs === animalTwo.numberOfLegs && animalOne.name < animalTwo.name) {
+    if (diffInNumberOfLegs === 0 && animalOne.name < animalTwo.name) {
       return -1;
     }
     return 1;
