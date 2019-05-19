b828f4ed51c0ad45c42913a7229cab91b6b8f770
amitbadala <amitbadala07@gmail.com>
2019-05-20 05:04:02 +0530

    Solve negative Index

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,23 +1,27 @@
- function negativeIndex(array) {
+function negativeIndex(array) {
+  if (!Array.isArray(array)) {
+    throw new TypeError("Only arrays are supported");
+  }
   var handler = {
-    get: function(target, propname) {
-      if(propname == 'length'){
-        return target.length;
-      } 
-      if(propname == 'symbol'){ 
-        return target;
+    get: function(target, index) {
+      if (index < 0) {
+        index = Number.parseInt(target.length, 10) + Number.parseInt(index, 10);
       }
+
+      return target[index];
     },
-    set:function(target,propname,value){
-
+    set: function(target, index, value) {
+      if (index < 0) {
+        index = Number.parseInt(target.length, 10) + Number.parseInt(index, 10);
+      }
+      target[index] = value;
+      return true;
     }
-};
-  return new Proxy(array,handler);
-
+  };
+  let result = new Proxy(array, handler);
+  return result;
 }
 
 // retutn Reflect.get(target,propname);
 // set(target,propname,__values)
-export {
-  negativeIndex,
-};
+export { negativeIndex };
