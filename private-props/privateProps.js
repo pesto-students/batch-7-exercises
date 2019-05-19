011d10565dd0a1ba6c01d33266fef69724919a40
manish zanzad <manishzanzad@gmail.com>
2019-05-19 23:45:09 +0530

    Solve excersice private Props

--- private-props/privateProps.js
+++ private-props/privateProps.js
@@ -1,8 +1,28 @@
-
-function privateProps(...args) {
-  return args;
+function privateProps(obj, privacyFilterFunc) {
+  const handler = {
+    get(target, prop) {
+      if (!privacyFilterFunc(prop)) {
+        if (typeof target[prop] === 'object') {
+          return new Proxy(target[prop], handler);
+        }
+        if (typeof target[prop] === 'function') {
+          return target[prop].bind(target);
+        }
+        return target[prop];
+      }
+      throw new Error(`Property ${prop} is private`);
+    },
+    has(target, prop) {
+      if (!privacyFilterFunc(prop)) {
+        return Reflect.has(target, prop);
+      }
+      return false;
+    },
+    ownKeys(target) {
+      const keys = Reflect.ownKeys(target);
+      return keys.filter(key => !privacyFilterFunc(key));
+    },
+  };
+  return new Proxy(obj, handler);
 }
-
-export {
-  privateProps,
-};
+export { privateProps };
