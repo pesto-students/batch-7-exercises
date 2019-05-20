1911b439ee83d7a3df5df78d0ce1a785f7b1d28e
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-20 08:57:01 +0530

    Refactored Aperture

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,14 +1,20 @@
 
-function aperture(n,array) {
+function aperture(n, array) {
   if (n > array.length) {
     return [];
-  } 
+  }
   let count = 0;
   const transformedArray = [];
-  while (count < array.length - n + 1)  { // Not going beyond length - size of each tuple
-    transformedArray.push(array.slice(count, count + n));
-    count += 1;
-  }
+  // while (count < array.length - n + 1)  { // Not going beyond length - size of each tuple
+  //   transformedArray.push(array.slice(count, count + n));
+  //   count += 1;
+  // }
+
+  array.forEach((element, index) => {
+    if (index < array.length - n + 1) {
+      transformedArray.push(array.slice(index, index + n))
+    }
+  });
   return transformedArray;
 }
 
