9f9b738b491bc53b153fd197eabef68fb35dbfd2
amitbadala <amitbadala07@gmail.com>
2019-05-21 01:39:09 +0530

    App.js

--- simple-post/frontend/src/App.js
+++ simple-post/frontend/src/App.js
@@ -1,11 +1,35 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
-import Post from './components/Post';
-import './App.css';
+import Post from "./components/Post";
+import "./App.css";
 
 class App extends Component {
   state = {
-    posts: [],
+    posts: []
+  };
+
+  apiUrl = "http://localhost:3001/posts/:page";
+  componentDidMount() {
+    this.getPost();
+  }
+
+  getPost = _ => {
+    let fetchData = {
+      method: "GET",
+      headers: new Headers({ "pesto-password": "darth vader" })
+    };
+    fetch(this.apiUrl, fetchData)
+      .then(response => {
+        console.log(response);
+        // if (response.data) {
+        //   this.setState({ posts: response.data });
+        // }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState({ posts: [data] });
+      });
   };
 
   render() {
@@ -14,14 +38,10 @@ class App extends Component {
         <h2>Posts</h2>
         <hr />
         <br />
-        <button onClick={this.handlePrevClick}>
-          Previous
-        </button>
-        <button onClick={this.handleNextClick}>
-          Next
-        </button>
+        <button onClick={this.handlePrevClick}>Previous</button>
+        <button onClick={this.handleNextClick}>Next</button>
         <div>
-          {this.state.posts.map((post) => {
+          {this.state.posts.map(post => {
             return (
               <Fragment key={post.id}>
                 <Post post={post} />
