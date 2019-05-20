a75fe6d307c3ba21e23463f0866fb476e6b40c49
VikalpP <vikalppanseriya@gmail.com>
2019-05-21 04:57:56 +0530

    WIP Simple-post

--- simple-post/frontend/src/App.js
+++ simple-post/frontend/src/App.js
@@ -1,12 +1,93 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Post from './components/Post';
 import './App.css';
 
 class App extends Component {
-  state = {
-    posts: [],
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pageNumber: 1,
+      posts: [],
+    };
+
+    this.handlePrevClick = this.handlePrevClick.bind(this);
+    this.handleNextClick = this.handleNextClick.bind(this);
+    this.fetchPagePosts = this.fetchPagePosts.bind(this);
+    this.getStorageKeyOfPostPaging = this.getStorageKeyOfPostPaging.bind(this);
+    this.handleData = this.handleData.bind(this);
+    this.reflectNewData = this.reflectNewData.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchPagePosts();
+  }
+
+  getStorageKeyOfPostPaging() {
+    const { pageNumber } = this.state;
+    return `PostOfPage: ${pageNumber}`;
+  }
+
+  fetchPagePosts() {
+    const { pageNumber } = this.state;
+    const { localStorage } = window;
+    const storageKey = this.getStorageKeyOfPostPaging();
+
+    const storageData = JSON.parse(localStorage.getItem(storageKey));
+    if (storageData) {
+      console.log('Storage data loaded!', storageData);
+
+      this.handleData(storageData);
+    } else {
+      console.log('Fetching new data!');
+
+      fetch(`http://localhost:3001/posts/${pageNumber}`, {
+        method: 'get',
+        headers: new Headers({
+          'pesto-password': 'darth vader',
+        }),
+      })
+        .then(data => data.json())
+        .then(({ data }) => {
+          this.handleData(data);
+          this.reflectNewData(data);
+        });
+    }
+  }
+
+  handleData(data) {
+    this.setState({ posts: data });
+  }
+
+  reflectNewData(data) {
+    const { pageNumber } = this.state;
+    const { localStorage } = window;
+    localStorage.setItem(`PostOfPage: ${pageNumber}`, JSON.stringify(data));
+  }
+
+  handlePrevClick() {
+    const { pageNumber } = this.state;
+    const requiredPageNumber = pageNumber > 0 ? pageNumber - 1 : 0;
+    this.setState(
+      {
+        pageNumber: requiredPageNumber,
+      },
+      () => this.fetchPagePosts(),
+    );
+  }
+
+  handleNextClick() {
+    const { pageNumber } = this.state;
+    this.setState(
+      {
+        pageNumber: pageNumber + 1,
+      },
+      () => this.fetchPagePosts(),
+    );
+  }
 
   render() {
     return (
@@ -14,21 +95,17 @@ class App extends Component {
         <h2>Posts</h2>
         <hr />
         <br />
-        <button onClick={this.handlePrevClick}>
-          Previous
-        </button>
-        <button onClick={this.handleNextClick}>
-          Next
-        </button>
+        <button onClick={this.handlePrevClick}>Previous</button>
+        <button onClick={this.handleNextClick}>Next</button>
         <div>
-          {this.state.posts.map((post) => {
-            return (
+          <Router>
+            {this.state.posts.map(post => (
               <Fragment key={post.id}>
                 <Post post={post} />
                 <hr />
               </Fragment>
-            );
-          })}
+            ))}
+          </Router>
         </div>
       </div>
     );
--- simple-post/frontend/src/components/Post.js
+++ simple-post/frontend/src/components/Post.js
@@ -1,20 +1,35 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
-const Post = ({ post }) => {
-  return (
-    <div key={post.id}>
-      <h4>{post.id}</h4>
-      <div>Title: {post.title}</div>
-    </div>
-  );
+const postTitleStyle = {
+  display: 'inline-block',
 };
+
+const Post = ({ post, history }) => (
+  <div
+    key={post.id}
+    role="button"
+    onClick={() => history.push(`post/${post.id}`)}
+    onKeyPress={() => history.push(`post/${post.id}`)}
+    tabIndex="0"
+  >
+    <h4>{post.id}</h4>
+    <div style={postTitleStyle}>
+      <h3>Title: {post.title}</h3>
+    </div>
+  </div>
+);
 
 Post.propTypes = {
   post: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
   }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
-export default Post;
+export default withRouter(Post);
