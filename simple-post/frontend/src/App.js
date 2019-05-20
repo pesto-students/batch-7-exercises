f3cd9e659c5f54f05b5f035cd50cc3aede6cfe6f
Pratiyush <pkpratiyush@gmail.com>
2019-05-21 01:05:34 +0530

    Solving Exercises day-16-segment-1

--- simple-post/frontend/src/App.js
+++ simple-post/frontend/src/App.js
@@ -4,11 +4,43 @@ import Post from './components/Post';
 import './App.css';
 
 class App extends Component {
-  state = {
-    posts: [],
-  };
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      page: 1
+    };
+
+    this.handleNextClick = this.handleNextClick.bind(this);
+    this.handlePrevClick = this.handlePrevClick.bind(this);
+  }
+
+  fetchPosts() {
+    const myheaders = new Headers();
+    myheaders.append('pesto-password','darth vader');
+    fetch(`http://localhost:3001/posts/${this.state.page}`,{headers: myheaders})
+    .then(response => response.json())
+    .then(res => this.setState({posts: res.data}));
+  }
+
+  componentDidMount() {
+    this.fetchPosts();
+  }
+
+  handlePrevClick() {
+    let value = this.state.page;
+    localStorage.setItem(`${this.state.page}`, `${this.setState({page: value --})}`);
+    this.fetchPosts();
+  }
+
+  handleNextClick() {
+    let value = this.state.page;
+    localStorage.setItem(`${this.state.page}`, `${this.setState({page: value ++})}`);
+    this.fetchPosts();
+  }
 
   render() {
+    console.log(this.state.posts);
     return (
       <div>
         <h2>Posts</h2>
@@ -19,7 +51,7 @@ class App extends Component {
         </button>
         <button onClick={this.handleNextClick}>
           Next
-        </button>
+        </button> {this.state.page}
         <div>
           {this.state.posts.map((post) => {
             return (
