f73da691c60a9876ddde582ad824705ff523e609
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-20 11:20:56 +0530

    WIP Context

--- advance-exercises/src/components/02-Context.js
+++ advance-exercises/src/components/02-Context.js
@@ -1,4 +1,3 @@
-
 /*
   Q2:
 
@@ -18,39 +17,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const FormContext = React.createContext();
+
+class FormContextProvider extends React.Component {
+  state = {
+    firstName: '',
+    lastName: ''
+  };
+
+  render() {
+    return (
+      <FormContext.Provider
+        value={{
+          state: this.state,
+          changeField: (fieldName, value) => {
+            const newState = {
+              ...this.state,
+              [fieldName]: value
+            };
+            this.setState(newState);
+          },
+          onSubmitHandler: () => {
+            console.log(`Form Content:\nFirst Name: ${this.state.firstName}\nLast Name: ${this.state.lastName}`);
+          }
+        }}
+      >
+        {this.props.children}
+      </FormContext.Provider>
+    );
+  }
+}
+
 class Form extends React.Component {
   static propTypes = {
-    children: PropTypes.shape().isRequired,
-  }
+    children: PropTypes.shape().isRequired
+  };
   render() {
-    return <div>{this.props.children}</div>;
+    return (<div>{this.props.children}</div>);
   }
 }
 
 class SubmitButton extends React.Component {
   static propTypes = {
-    children: PropTypes.shape().isRequired,
-  }
+    children: PropTypes.shape().isRequired
+  };
   render() {
-    return <button>{this.props.children}</button>;
+    return <FormContext.Consumer>
+    {context => <button onClick={context.onSubmitHandler}>{this.props.children}</button>}
+  </FormContext.Consumer>
+    
   }
 }
 
 class TextInput extends React.Component {
   render() {
     return (
-      <input
-        type="text"
-        name={this.props.name}
-        placeholder={this.props.placeholder}
-      />
+      <FormContext.Consumer>
+        {context => (
+          <input
+            type="text"
+            name={this.props.name}
+            placeholder={this.props.placeholder}
+            value={context.state[this.props.name]}
+            onChange={(event, fieldName = this.props.name) =>
+              context.changeField(fieldName, event.target.value)
+            }
+          />
+        )}
+      </FormContext.Consumer>
     );
   }
 }
 
 TextInput.propTypes = {
   name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired
 };
 
 class Context extends React.Component {
@@ -65,15 +106,17 @@ class Context extends React.Component {
           This isn&#39;t even my final <code>&lt;Form/&gt;</code>!
         </h1>
 
-        <Form onSubmit={this.handleSubmit}>
-          <p>
-            <TextInput name="firstName" placeholder="First Name" />{' '}
-            <TextInput name="lastName" placeholder="Last Name" />
-          </p>
-          <p>
-            <SubmitButton>Submit</SubmitButton>
-          </p>
-        </Form>
+        <FormContextProvider>
+          <Form onSubmit={this.handleSubmit}>
+            <p>
+              <TextInput name="firstName" placeholder="First Name" />{' '}
+              <TextInput name="lastName" placeholder="Last Name" />
+            </p>
+            <p>
+              <SubmitButton>Submit</SubmitButton>
+            </p>
+          </Form>
+        </FormContextProvider>
       </div>
     );
   }
