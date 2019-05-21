ce576812899a69de91b25fdbd97d70d87d379fa1
manish zanzad <manishzanzad@gmail.com>
2019-05-22 01:52:37 +0530

    Solve excersice filtered-names-recursive

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,8 +1,23 @@
+const fs = require('fs');
+const path = require('path');
 
-function filteredNamesRecursive(...args) {
-  return args;
+function filteredNamesRecursive(directoryPath, fileExt) {
+  const fileExtToLower = fileExt.toLowerCase();
+  let matchedFiles = [];
+  const files = fs.readdirSync(directoryPath, { withFileTypes: true });
+  files.forEach((file) => {
+    if (file.isDirectory()) {
+      const nestedDirPath = path.join(directoryPath, file.name);
+      matchedFiles = matchedFiles.concat(filteredNamesRecursive(nestedDirPath, fileExtToLower));
+    } else {
+      const splitFileName = file.name.split('.');
+      const ext = splitFileName[splitFileName.length - 1];
+      if (ext.toLowerCase() === fileExtToLower) {
+        matchedFiles.push(file.name);
+      }
+    }
+  });
+
+  return matchedFiles;
 }
-
-export {
-  filteredNamesRecursive,
-};
+export { filteredNamesRecursive };
