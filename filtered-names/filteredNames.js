476fdc5f9727ff9eabc35dba27b0055ac2def603
VikalpP <vikalppanseriya@gmail.com>
2019-05-22 01:04:11 +0530

    Solve filteredNames

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
 
-function filteredNames(...args) {
-  return args;
+const getExtension = (fileName) => {
+  const index = fileName.lastIndexOf('.');
+  return index === -1 ? '' : fileName.substr(index + 1);
+};
+
+function filteredNames(dirname, fileExtension) {
+  const listOfFileNames = fs.readdirSync(dirname);
+
+  const filteredFiles = listOfFileNames.filter(
+    fileName => getExtension(fileName) === fileExtension,
+  );
+
+  return filteredFiles.sort();
 }
 
-export {
-  filteredNames,
-};
+export { filteredNames };
