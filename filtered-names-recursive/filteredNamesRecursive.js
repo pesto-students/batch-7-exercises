c3b0d263daed5fdc9b039ea876df55fef1dae3da
VikalpP <vikalppanseriya@gmail.com>
2019-05-22 01:12:25 +0530

    WIP filteredNamesRecursive

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,8 +1,29 @@
+const fs = require('fs');
 
-function filteredNamesRecursive(...args) {
-  return args;
+const getExtension = (fileName) => {
+  const index = fileName.lastIndexOf('.');
+  return index === -1 ? '' : fileName.substr(index + 1);
+};
+
+function filteredNamesRecursive(dirname, fileExtension) {
+  // TODO check for dir and recursive logic 
+  // const listOfFileNames = fs.readdirSync(dirname);
+
+  let dirs = [dirname];
+  const listOfFileNames 
+
+  while(dirs) {
+    const dirName = dirs.pop()
+    const newFiles = fs.readdirSync(dirName);
+    listOfFileNames.push(...newFiles);
+    listOfFileNames.filter(files => )
+  }
+
+  const filteredFiles = listOfFileNames.filter(
+    fileName => getExtension(fileName) === fileExtension,
+  );
+
+  return filteredFiles.sort();
 }
 
-export {
-  filteredNamesRecursive,
-};
+export { filteredNamesRecursive };
