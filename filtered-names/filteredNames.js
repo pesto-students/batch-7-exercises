868e06347f16f24f4195f6cdcceb6e7967235ed1
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-22 01:06:52 +0530

    Revert "[WIP] filtered-names"
    This reverts commit 6a39271104fbfbd024cb2c4270f5e5936fd7e00f.

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,15 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-function filteredNames(directoryPath, fileType) {
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(files);
-    const filteredFiles= files.filter((fileName) => fileName.includes(fileType) ? fileName : '')
-    console.log(filteredFiles);
-    return filteredFiles;
-  });
+
+function filteredNames(...args) {
+  return args;
 }
 
 export {
