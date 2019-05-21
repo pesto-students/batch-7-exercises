98683c11ab26f96443e9575df9e07680dc762f85
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-22 01:43:38 +0530

    Solve filteredNamesRecursively

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,8 +1,34 @@
+const fs = require('fs');
+const path = require('path');
 
-function filteredNamesRecursive(...args) {
-  return args;
+const allFiles = [];
+
+function getFilesWithExtension(files, fileExtension) {
+  const matchingFiles = [];
+  files.forEach((fileName) => {
+    if (fileName.includes(fileExtension)) {
+      matchingFiles.push(fileName);
+    }
+  });
+  return matchingFiles;
 }
 
-export {
-  filteredNamesRecursive,
-};
+function getFilesRecursively(directory) {
+  const listOfFiles = fs.readdirSync(directory);
+  listOfFiles.forEach((file) => {
+    const filePath = path.resolve(directory, file);
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      getFilesRecursively(filePath);
+    } else {
+      allFiles.push(file);
+    }
+  });
+}
+
+function filteredNamesRecursive(directory, fileExtension) {
+  getFilesRecursively(directory);
+  return getFilesWithExtension(allFiles, fileExtension);
+}
+
+export { filteredNamesRecursive };
