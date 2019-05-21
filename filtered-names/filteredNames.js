91b6c9a48e74ae1930d5a07cda13e32dd42e22f5
manish zanzad <manishzanzad@gmail.com>
2019-05-22 01:01:35 +0530

    Solve excersice filtered-names

--- filtered-names/filteredNames.js
+++ filtered-names/filteredNames.js
@@ -1,21 +1,18 @@
-const path = require('path');
-
 const fs = require('fs');
 
 function filteredNames(directoryPath, fileExt) {
   const fileExtToLower = fileExt.toLowerCase();
   const matchedFiles = [];
-  fs.readdirSync(directoryPath, (error, files) => {
-    files.forEach((file) => {
-      const splitFileName = file.split('.');
-      const ext = splitFileName[splitFileName.length - 1];
-      if (ext.toLowerCase() === fileExtToLower) {
-        matchedFiles.push(file);
-      }
-    });
+  const files = fs.readdirSync(directoryPath);
+  files.forEach((file) => {
+    const splitFileName = file.split('.');
+    const ext = splitFileName[splitFileName.length - 1];
+    if (ext.toLowerCase() === fileExtToLower) {
+      matchedFiles.push(file);
+    }
   });
 
   return matchedFiles;
 }
-console.log(filteredNames(path.join(__dirname, 'Directory'), 'json'));
-// export { filteredNames };
+
+export { filteredNames };
