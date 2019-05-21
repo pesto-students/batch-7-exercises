a6feaefc1b6193a65d129f032f58bc01e95a1008
Pratiyush <pkpratiyush@gmail.com>
2019-05-22 01:53:34 +0530

    Solving exercises day-17 segment-1

--- json-response-server/jsonResponseServer.js
+++ json-response-server/jsonResponseServer.js
@@ -1,6 +1,7 @@
 import http from 'http';
 const jsonResponseServer = http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify({ data: 'Pesto Bootcamp!'}));
   res.end();
 });
 
