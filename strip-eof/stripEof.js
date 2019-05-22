0ecaf3a782d238def976e9a3e6f447c4ba1adae2
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-22 08:50:13 +0530

    Solve stripEof

--- strip-eof/stripEof.js
+++ strip-eof/stripEof.js
@@ -1,8 +1,27 @@
+const CARRIAGE_RETURN_CHAR = '\r';
+const NEW_LINE_CHAR = '\n';
 
-function stripEof(...args) {
-  return args;
+function stripEof(string) {
+  let chars;
+  if (Buffer.isBuffer(string)) {
+    chars = string.toString('utf8').split('');
+  } else {
+    chars = string.split('');
+  }
+  const lastElement = chars[chars.length - 1];
+  const secondLastElement = chars[chars.length - 2];
+  const hasEndOfLine = lastElement === CARRIAGE_RETURN_CHAR || lastElement === NEW_LINE_CHAR;
+  const hasBothCharactersAtEnd = (secondLastElement === CARRIAGE_RETURN_CHAR
+  && lastElement === NEW_LINE_CHAR);
+  if (hasBothCharactersAtEnd) {
+    chars.splice(chars.length - 2, 2, '');
+    return chars.join('');
+  }
+  if (hasEndOfLine) {
+    chars.splice(chars.length - 1, 1, '');
+    return chars.join('');
+  }
+  return string;
 }
 
-export {
-  stripEof,
-};
+export { stripEof };
