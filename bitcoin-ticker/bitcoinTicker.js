0acd2705075bdc40c60f3dabf169e01f3fae3764
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-22 12:35:33 +0530

    Solve bitcoinTicker

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,8 +1,32 @@
+const http = require('https');
 
-function bitcoinTicker(...args) {
-  return args;
-}
-
-export {
-  bitcoinTicker,
+const options = {
+  hostname: 'api.coinmarketcap.com',
+  path: '/v2/ticker/',
+  method: 'GET',
 };
+
+function bitcoinTicker({ limit }) {
+  return new Promise((resolve) => {
+    http.get(options, (res) => {
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => {
+        rawData += chunk;
+      });
+      res.on('end', () => {
+        const jsonData = JSON.parse(rawData);
+        const coins = jsonData.data;
+        const topCoins = [];
+        for (const key in coins) {
+          if (coins[key].rank <= limit) {
+            topCoins.push(coins[key].name);
+          }
+        }
+        console.log(topCoins);
+        resolve(jsonData);
+      });
+    });
+  });
+}
+export { bitcoinTicker };
