a1cc19d5892dfebbe643f37ffa12a3addfe3c8f8
VikalpP <vikalppanseriya@gmail.com>
2019-05-22 11:17:58 +0530

    WIP threeSum

--- three-sum/threeSum.js
+++ three-sum/threeSum.js
@@ -1,5 +1,16 @@
 function threeSum(array, sum) {
   const { length } = array;
+
+  if (length < 3) {
+    throw new Error(
+      `Expected first param to be array with at least 3 numbers, Received [${array}]`,
+    );
+  }
+
+  if (Number.isNaN(sum)) {
+    throw new Error(`Expected second param to be number, Received ${typeof sum}`);
+  }
+
   const foundIndexes = [];
   for (let i = 0; i < length; i += 1) {
     for (let j = i + 1; j < length; j += 1) {
@@ -10,6 +21,8 @@ function threeSum(array, sum) {
       }
     }
   }
+
+  if (foundIndexes.length === 0) return null;
 
   const sorted = foundIndexes.sort((a, b) => a.i + a.j + a.k - (b.i + b.j + b.k));
   const { i, j, k } = sorted[0];
