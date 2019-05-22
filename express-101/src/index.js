eec391016ebfd38ff786a65a76d77454cecd61f9
Pratiyush <pkpratiyush@gmail.com>
2019-05-23 00:56:23 +0530

    Solving day-18 Segment-1 exercises

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -6,4 +6,10 @@ const app = express();
 
 // 2) Start server on port 3000
 
+app.get('/', (req,res) => res.send("Hey, I am server response"));
+
+app.get('/movie/:title', (req,res) => res.send(`My favorite movie is ${req.params.title} `));
+
+app.listen(3000, () => console.log('Server listening on port: 3000'));
+
 module.exports = app;
