7f08d351181225a81530ecb340abd1c99b036298
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-22 09:43:20 +0530

    WIP validFilename

--- valid-filename/validFilename.js
+++ valid-filename/validFilename.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
-function validFilename(...args) {
-  return args;
+function validFilename(fileName) {
+  const filePath = path.resolve(__dirname, fileName);
+  if (fileName.length === 0) {
+    return false;
+  }
+  if (fs.existsSync(filePath)) {
+    return false;
+  }
+  return true;
 }
 
-export {
-  validFilename,
-};
+export { validFilename };
