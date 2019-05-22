d63d03a4e59fbb157e562e32ef1bfcdb03c683c1
VikalpP <vikalppanseriya@gmail.com>
2019-05-22 11:05:59 +0530

    WIP threeSum

--- three-sum/threeSum.js
+++ three-sum/threeSum.js
@@ -1,8 +1,19 @@
+function threeSum(array, sum) {
+  const { length } = array;
+  const foundIndexes = [];
+  for (let i = 0; i < length; i += 1) {
+    for (let j = i + 1; j < length; j += 1) {
+      for (let k = j + 1; k < length; k += 1) {
+        if (array[i] + array[j] + array[k] === sum) {
+          foundIndexes.push({ i, j, k });
+        }
+      }
+    }
+  }
 
-function threeSum(...args) {
-  return args;
+  const sorted = foundIndexes.sort((a, b) => a.i + a.j + a.k - (b.i + b.j + b.k));
+  const { i, j, k } = sorted[0];
+  return [array[i], array[j], array[k]];
 }
 
-export {
-  threeSum,
-};
+export { threeSum };
