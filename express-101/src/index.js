4b073f8a3f2d97ef463dc12a5591bdd1436bc963
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-23 00:29:06 +0530

    Solve express-101

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-// 1) Create Routes
+app.get('/', (request, response) => {
+  response.send('Hey, I am server response');
+});
 
-// 2) Start server on port 3000
+app.get('/movie/:title', (request, response) => {
+  response.send(`My favorite movie is ${request.params.title}`);
+});
+
+app.listen(3000);
 
 module.exports = app;
