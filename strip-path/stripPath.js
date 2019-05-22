97d9fb9caa445ac8efa8d141dc8a02e5e6cdfd73
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-22 13:13:25 +0530

    Solve stripPath

--- strip-path/stripPath.js
+++ strip-path/stripPath.js
@@ -1,8 +1,11 @@
+const path = require('path');
 
-function stripPath(...args) {
-  return args;
+function stripPath(pathString, from) {
+  const relativePath = path.relative('./', pathString);
+  if (typeof from !== 'string') {
+    return pathString;
+  }
+  return path.relative(from, relativePath);
 }
 
-export {
-  stripPath,
-};
+export { stripPath };
