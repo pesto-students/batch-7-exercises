520c7e904914ffbe5ea441ded0b9d4fd9ce5901a
manish zanzad <manishzanzad@gmail.com>
2019-05-23 00:58:59 +0530

    Solve excersice express-forms

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
+app.get('/', (req, res) => res.redirect('/form-with-get'));
+
+app.get('/form-with-get', (req, res) => res.render('form-with-get'));
+
+app.get('/form-with-post', (req, res) => res.render('form-with-post'));
+
+app.get('/submit-form-with-get', (req, res) => {
+  res.send({ firstName: req.query.first, lastName: req.query.last });
 });
 
-app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
+app.post('/submit-form-with-post', (req, res) => {
+  res.send({ firstName: req.body.first, lastName: req.body.last });
 });
 
-app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
-});
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.listen(3001, () => {
+  console.log('Server is listening on port 3001');
 });
