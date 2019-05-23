65dcab888193acf62231bc57a96ddd0f9c5f8afc
manish zanzad <manishzanzad@gmail.com>
2019-05-23 18:38:20 +0530

    Solve excersice splitEvery

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,8 +1,13 @@
-
-function splitEvery(...args) {
-  return args;
+function splitEvery(subSetCount, data) {
+  if (subSetCount === 0 || subSetCount === -1) {
+    throw new Error('Invalid first argument, should be positive');
+  }
+  const splitData = [];
+  const len = data.length;
+  for (let i = 0; i < len; i += subSetCount) {
+    splitData.push(data.slice(i, i + subSetCount));
+  }
+  return splitData;
 }
 
-export {
-  splitEvery,
-};
+export { splitEvery };
