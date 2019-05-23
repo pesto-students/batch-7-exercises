ac0dbd8c48cde6478dccc4640d1747d3899255cd
manish zanzad <manishzanzad@gmail.com>
2019-05-23 17:17:58 +0530

    Solve excersice xprod

--- xprod/xprod.js
+++ xprod/xprod.js
@@ -1,8 +1,16 @@
-
-function xprod(...args) {
-  return args;
+function xprod(array1, array2) {
+  if (array1.length === 0 || array1.length === 0) {
+    return [];
+  }
+  const combinationOfArray = [];
+  array1.map((ele) => {
+    array2.map((eleOfarr2) => {
+      combinationOfArray.push([ele, eleOfarr2]);
+      return eleOfarr2;
+    });
+    return ele;
+  });
+  return combinationOfArray;
 }
 
-export {
-  xprod,
-};
+export { xprod };
