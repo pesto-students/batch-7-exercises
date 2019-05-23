962096ba411ba9eb1ce54e4b12b97a93d647188f
manish zanzad <manishzanzad@gmail.com>
2019-05-23 15:24:36 +0530

    Solve excersice on-change

--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,8 +1,30 @@
+function onChange(object, callback) {
+  const handler = {
+    get(target, prop) {
+      try {
+        if (typeof target[prop] === 'function' && prop === 'sort') {
+          callback();
+        }
+        return new Proxy(target[prop], handler);
+      } catch (err) {
+        return Reflect.get(target, prop);
+      }
+    },
+    set(target, prop, value) {
+      callback();
+      return Reflect.set(target, prop, value);
+    },
+    defineProperty(target, prop, descriptor) {
+      callback();
+      return Reflect.defineProperty(target, prop, descriptor);
+    },
+    deleteProperty(target, prop) {
+      callback();
+      return Reflect.deleteProperty(target, prop);
+    },
+  };
 
-function onChange(...args) {
-  return args;
+  return new Proxy(object, handler);
 }
 
-export {
-  onChange,
-};
+export { onChange };
