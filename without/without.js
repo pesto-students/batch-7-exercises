564e340305ef3cd738ced1d4be344502ddd6db18
manish zanzad <manishzanzad@gmail.com>
2019-05-23 17:11:22 +0530

    Solve excersice without

--- without/without.js
+++ without/without.js
@@ -1,8 +1,25 @@
+function without(array1, array2) {
+  const map = new Map(Object.entries({ ...array1 }));
+  array1.map((ele) => {
+    if (Object.is(ele, -0)) {
+      map.set('-0', ele);
+    } else if (typeof ele === 'object') {
+      map.set(ele.toString(), ele);
+    } else {
+      map.set(ele, ele);
+    }
+    return ele;
+  });
 
-function without(...args) {
-  return args;
+  return array2.filter((ele) => {
+    if (Object.is(ele, -0)) {
+      return !map.has('-0');
+    }
+    if (typeof ele === 'object') {
+      return !map.has(ele.toString());
+    }
+    return !map.has(ele);
+  });
 }
 
-export {
-  without,
-};
+export { without };
