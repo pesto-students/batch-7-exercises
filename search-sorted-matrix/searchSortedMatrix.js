ec9b27932f632c3e19b4ac87ff5ee3fa0865578e
manish zanzad <manishzanzad@gmail.com>
2019-05-23 17:54:14 +0530

    Solve excersice serached-sort-matrix

--- search-sorted-matrix/searchSortedMatrix.js
+++ search-sorted-matrix/searchSortedMatrix.js
@@ -1,8 +1,41 @@
-
-function searchSortedMatrix(...args) {
-  return args;
+function binarySearch(key, array) {
+  let startIndex = 0;
+  let lastIndex = array.length - 1;
+  while (startIndex <= lastIndex) {
+    const middle = parseInt((startIndex + lastIndex) / 2, 10);
+    if (array[middle] === key) {
+      return middle;
+    }
+    if (key < array[middle]) {
+      lastIndex = middle - 1;
+    } else {
+      startIndex = middle + 1;
+    }
+  }
+  return -1;
 }
 
-export {
-  searchSortedMatrix,
-};
+function searchSortedMatrix({ search, matrix }) {
+  const rowLen = matrix.length;
+  const colLen = matrix[0].length;
+  const lastCol = colLen - 1;
+  let possibleRowIndex = -1;
+  for (let i = 0; i < rowLen; i += 1) {
+    const row = matrix[i];
+    if (search <= row[lastCol]) {
+      possibleRowIndex = i;
+      break;
+    }
+  }
+  if (possibleRowIndex === -1) {
+    return false;
+  }
+
+  const index = binarySearch(search, matrix[possibleRowIndex]);
+  if (index !== -1) {
+    return true;
+  }
+  return false;
+}
+
+export { searchSortedMatrix };
