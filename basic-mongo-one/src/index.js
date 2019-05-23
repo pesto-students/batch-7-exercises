d32fea69228bb6eff3b58548090b3a55618a5296
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-24 04:44:58 +0530

    [WIP] SOlving queries

--- basic-mongo-one/src/index.js
+++ basic-mongo-one/src/index.js
@@ -1,14 +1,24 @@
+const getDb = require('./database').getDb;
 /* Q1 (*)
   Return the number of movies in the "movies" collection without using array.length
 */
-const getMoviesCount = async () => {};
+const getMoviesCount = async () => {
+  try {
+    const db = await getDb();
+    const count = await db.collection('movies').find({}).count();
+    return count;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+getMoviesCount();
 /* Q2 (*)
   Return the first movie with imdb rating = 9.2 and year = 1974.
   Also, use mongodb projections to only get title from mongodb as opposed
   to accessing title property from the object
 */
-const movieRating = async () => {};
+const movieRating = async () => { };
 
 /* Q3 (*)
   Return the number of movies written by all these people (exactly these people in this order):
@@ -17,75 +27,75 @@ const movieRating = async () => {};
   Damon Lindelof
   Gene Roddenberry
 */
-const writersIntersection = async () => {};
+const writersIntersection = async () => { };
 
 /* Q4 (*)
   Return the number of movies written by any of the writers in Q3
 */
-const writersUnion = async () => {};
+const writersUnion = async () => { };
 
 /* Q5 (*)
   Return the number of movies in which actor is "Jackie Chan"
 */
-const actor = async () => {};
+const actor = async () => { };
 
 /* Q6 (*)
   Return the number of movies in which actor "Jackie Chan" is second
   in the array "actors"
 */
-const positionalActor = async () => {};
+const positionalActor = async () => { };
 
 /* Q7 (*)
   Return the first movie with imdb rating greater than or equal to 9.0
   and less than or equal to 9.2
 */
-const comparisonOperator = async () => {};
+const comparisonOperator = async () => { };
 
 /* Q8 (*)
   Return the number of movies which have an actual rating opposed to
   being "UNRATED" or having no "rated" field at all
 */
-const trimUnrated = async () => {};
+const trimUnrated = async () => { };
 
 /* Q9 (*)
   Return number of movies in which "tomato" field exists but "tomato.rating" does not
 */
-const unratedByTomato = async () => {};
+const unratedByTomato = async () => { };
 
 /* Q10 (*)
   Return number of movies with higher imdb rating >= 9.0 OR
   metacritic >= 90
 */
-const goodMovies = async () => {};
+const goodMovies = async () => { };
 
 /* Q11 (*)
   Return number of movies where tomato field exists AND
   is equal to null
 */
-const regexSearch = async () => {};
+const regexSearch = async () => { };
 
 /* Q12 (*)
   Return number of movies where 'Adventure' and 'Action'
   as genres in any order
 */
-const arrayAll = async () => {};
+const arrayAll = async () => { };
 
 /* Q13 (*)
   Return number of movies that were filmed in exactly 4 countries
 */
-const fieldArraySize = async () => {};
+const fieldArraySize = async () => { };
 
 /* Q14 (*)
   Add a field called "myRating" = 90 to the movie "Iron Man 3" in movieDetails collection
 */
-const addField = async () => {};
+const addField = async () => { };
 
 /* Q15 (*)
   Increment the metacritic rating by 5 for the movie "Gone Girl" with a single query.
   Note: Do not use find() or findOne() to look for the current metacritic rating for "Gone Girl"
 */
-const incrementalUpdate = async () => {};
+const incrementalUpdate = async () => { };
 
 module.exports = {
-  getMoviesCount, 
+  getMoviesCount,
 };
