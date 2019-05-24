c40deb748cd4ede4aa0207e811c6ff28540ac356
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-25 01:04:15 +0530

    WIP simple-todo-tdd

---
+++ simple-todo-tdd/client/client.test.js
@@ -0,0 +1,15 @@
+describe('client test', () => {
+  it('should render an input box to add new todos', () => {});
+
+  it('should render a list of todos', () => {});
+
+  it('should render an Add Task button', () => {});
+
+  it('should render a new page on Add Task button', () => {});
+
+  it('should render an delete button against every task', () => {});
+
+  it('should render an edit button against every task', () => {})
+
+  it('should change the todo item to input box on clicking edit button', () => {})
+});
---
+++ simple-todo-tdd/server/server.test.js
@@ -0,0 +1,21 @@
+describe('server tests', () => {
+  it('should check if server is up', async () => {
+    // check for server response code 200
+  });
+
+  it('should check if connected to database', () => {});
+
+  it('should get all todos', () => {});
+
+  it('should get a particular todo by id', () => {});
+
+  it('should add a new todo', () => {});
+
+  it('should delete a new todo by id', () => {});
+
+  it('should edit a todo by id', () => {});
+
+  it('should complete a todo by id', () => {});
+
+  it('should undo completion of todo by id', () => {});
+});
