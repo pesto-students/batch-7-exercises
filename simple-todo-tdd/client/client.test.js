e7c78e23e4898a4f7327e401006a42d4427b3a28
Pratiyush <pkpratiyush@gmail.com>
2019-05-25 00:58:27 +0530

    exercises day-20-segment-1

---
+++ simple-todo-tdd/client/client.test.js
@@ -0,0 +1,13 @@
+describe('client test', () => {
+    it('should render an input box to add new todos', () => {})
+
+    it('should render a list of todos', () => {})
+
+    it('should render ')
+
+    it('should render a new page on Add button, () => {})
+
+    it('', () => {})
+
+
+})
