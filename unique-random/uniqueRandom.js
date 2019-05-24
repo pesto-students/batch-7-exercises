d1e0199e1c052a7641bcd9ddd419a2ed669dfbc8
VikalpP <vikalppanseriya@gmail.com>
2019-05-25 01:00:48 +0530

    Solve uniqueRandom

--- unique-random/uniqueRandom.js
+++ unique-random/uniqueRandom.js
@@ -1,8 +1,17 @@
+const getRandomGenerator = (min, max) => () => Math.floor(Math.random() * max) + min;
 
-function uniqueRandom(...args) {
-  return args;
+function uniqueRandom(lowerBound, upperBound) {
+  const getRandom = getRandomGenerator(lowerBound, upperBound);
+  let prevRandom = getRandom();
+
+  return () => {
+    let newRandom;
+    do {
+      newRandom = getRandom();
+    } while (newRandom === prevRandom);
+    prevRandom = newRandom;
+    return newRandom;
+  };
 }
 
-export {
-  uniqueRandom,
-};
+export { uniqueRandom };
