09658fb5a7c3db33acf5aa9417d32ac0e11e6c67
manish zanzad <manishzanzad@gmail.com>
2019-05-24 13:53:18 +0530

    Solve excersice sudanFn

--- sudan-fn/sudanFn.js
+++ sudan-fn/sudanFn.js
@@ -1,8 +1,15 @@
-
-function sudanFn(...args) {
-  return args;
+function sundanFn({ n, x, y }) {
+  if (n === 0) {
+    return x + y;
+  }
+  if (y === 0) {
+    return x;
+  }
+  return sundanFn({
+    n: n - 1,
+    x: sundanFn({ n, x, y: y - 1 }),
+    y: sundanFn({ n, x, y: y - 1 }) + y,
+  });
 }
 
-export {
-  sudanFn,
-};
+export { sundanFn };
