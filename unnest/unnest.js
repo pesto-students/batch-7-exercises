e68a4d66173fa4af8b7c641120e1930ffae5aa18
manish zanzad <manishzanzad@gmail.com>
2019-05-24 16:52:07 +0530

    Solve excersice unnest

--- unnest/unnest.js
+++ unnest/unnest.js
@@ -1,8 +1,29 @@
+function unnest(data) {
+  let unnestedRes = [];
+  const array = data;
+  if (typeof array === 'object' && !Array.isArray(array)) {
+    array[Symbol.iterator] = function () {
+      let i = -1;
+      return {
+        next() {
+          i += 1;
+          if (i < array.length) {
+            return { value: array[i], done: false };
+          }
+          return { value: undefined, done: true };
+        },
+      };
+    };
+  }
 
-function unnest(...args) {
-  return args;
+  for (const ele of array) {
+    if (Array.isArray(ele)) {
+      unnestedRes = unnestedRes.concat(ele);
+    } else {
+      unnestedRes.push(ele);
+    }
+  }
+  return unnestedRes;
 }
 
-export {
-  unnest,
-};
+export { unnest };
