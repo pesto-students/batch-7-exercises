851846db42ae6bcb00a5c44ca9aa344231b34d8d
manish zanzad <manishzanzad@gmail.com>
2019-05-24 20:31:34 +0530

    Solve excersice primality-test

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,8 +1,13 @@
-
-function primalityTest(...args) {
-  return args;
+function primalityTest(number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-export {
-  primalityTest,
-};
+export { primalityTest };
