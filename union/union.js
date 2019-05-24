edc306b37137746c919f9139cd89864df25f524f
manish zanzad <manishzanzad@gmail.com>
2019-05-24 16:09:08 +0530

    Solve excersice uniq

--- union/union.js
+++ union/union.js
@@ -1,8 +1,11 @@
-
-function union(...args) {
-  return args;
+function union(m, n) {
+  const array1 = m;
+  const array2 = n;
+  let index = array1.indexOf(-0);
+  array1[index] = '-0';
+  index = array2.indexOf(-0);
+  array2[index] = '-0';
+  return Array.from(new Set([...array1, ...array2]));
 }
 
-export {
-  union,
-};
+export { union };
--- uniq/uniq.js
+++ uniq/uniq.js
@@ -1,8 +1,13 @@
-
-function uniq(...args) {
-  return args;
+function uniq(array) {
+  for (const key in array) {
+    if (Object.is(array[key], -0)) {
+      array[key] = '-0';
+    }
+    if (array[key] && typeof array[key] === 'object') {
+      array[key] = Object.entries(array[key]).toString();
+    }
+  }
+  return Array.from(new Set([...array]));
 }
 
-export {
-  uniq,
-};
+export { uniq };
