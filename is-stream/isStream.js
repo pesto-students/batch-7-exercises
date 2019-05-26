47937e3ca7ff5df32cf6c4df5d5f51c78e28cd1b
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-26 07:28:24 +0530

    Solve isStream

--- is-stream/isStream.js
+++ is-stream/isStream.js
@@ -1,8 +1,12 @@
+import { Stream } from 'stream';
 
-function isStream(...args) {
-  return args;
-}
+const isStream = object => object instanceof Stream.Stream;
 
-export {
-  isStream,
-};
+isStream['writable'] = obj => (obj instanceof Stream.Writable && obj.writable == true && typeof obj._writableState == 'object');
+
+isStream['readable'] = obj => ( obj instanceof Stream.Readable && obj.readable == true && typeof obj._readableState == 'object');
+
+isStream['duplex'] = obj => ( obj instanceof Stream.Duplex);
+
+isStream['transform'] = obj => ( obj instanceof Stream.Transform);
+export { isStream };
