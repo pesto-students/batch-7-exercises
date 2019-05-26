e257871b5e10cd038fd01d48d21bc08dd1090690
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-05-26 13:53:23 +0530

    Solve square-numbers-array

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,8 +1,10 @@
-
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(array) {
+  return array.map((el) => {
+    if (typeof el !== 'number') {
+      throw new Error(`Expected array of numbers. Found an element of type ${typeof el}`);
+    }
+    return el ** 2;
+  });
 }
 
-export {
-  squareNumbersArray,
-};
+export { squareNumbersArray };
