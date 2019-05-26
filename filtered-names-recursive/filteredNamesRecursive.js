8b677f7f1c6a8b52d692de98be0768b77ff66479
Pravesh Tora <pravesh.tora@gmail.com>
2019-05-26 07:30:44 +0530

    Solve file-name-recursive

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,6 +1,20 @@
+import path from 'path';
+import fs from 'fs';
+const filterFiles = [];
+function filteredNamesRecursive(dir, fileType) {
 
-function filteredNamesRecursive(...args) {
-  return args;
+  const files = fs.readdirSync(dir);
+  files.forEach((fileName) => {
+    const absoulteFileName = `${dir}/${fileName}`;
+    const stat = fs.statSync(absoulteFileName);
+    if (stat && stat.isDirectory()) {
+      filteredNamesRecursive(absoulteFileName, fileType);
+    }
+    if (fileName.includes(fileType)) {
+      filterFiles.push(fileName);
+    }
+  });
+  return filterFiles;
 }
 
 export {
